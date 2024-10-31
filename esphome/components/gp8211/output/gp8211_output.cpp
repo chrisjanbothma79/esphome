f96c4e6875b8a023a7bfd67312a39bf151ae4941
haudamekki <38363966+haudamekki@users.noreply.github.com>
2024-10-31 08:09:20 +0100

    Update esphome/components/gp8211/output/gp8211_output.cpp
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- esphome/components/gp8211/output/gp8211_output.cpp
+++ esphome/components/gp8211/output/gp8211_output.cpp
@@ -11,7 +11,6 @@ static const uint8_t OUTPUT_REGISTER = 0x02;
 void GP8211Output::dump_config() { ESP_LOGCONFIG(TAG, "GP8211 Output:"); }
 
 void GP8211Output::write_state(float state) {
-  ESP_LOGD(TAG, "Original brightness state received from Light component: %.5f", state);
 
   uint16_t value = static_cast<uint16_t>(state * 32767);
   ESP_LOGD(TAG, "Calculated DAC value: %u", value);
