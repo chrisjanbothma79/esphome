c84ffd3a21e0cd8d2f545578e79c09280d132b03
mikosoft83 <63317931+mikosoft83@users.noreply.github.com>
2024-11-02 23:04:28 +0100

    Update display.cpp

--- esphome/components/display/display.cpp
+++ esphome/components/display/display.cpp
@@ -662,7 +662,7 @@ void DisplayOnPageChangeTrigger::process(DisplayPage *from, DisplayPage *to) {
   if ((this->from_ == nullptr || this->from_ == from) && (this->to_ == nullptr || this->to_ == to))
     this->trigger(from, to);
 }
-void Display::strftime(int x, int y, BaseFont *font, Color color, Color background, TextAlign align, const char *format, 
+void Display::strftime(int x, int y, BaseFont *font, Color color, Color background, TextAlign align, const char *format,
                        ESPTime time) {
   char buffer[64];
   size_t ret = time.strftime(buffer, sizeof(buffer), format);
