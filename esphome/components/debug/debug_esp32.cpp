7c2cdcfea38833945d4204fe76b49ebf4e1fdf7f
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2024-11-07 00:24:43 +0100

    I forgot the `;`
    No comments, please. :stuck_out_tongue_winking_eye:

--- esphome/components/debug/debug_esp32.cpp
+++ esphome/components/debug/debug_esp32.cpp
@@ -88,55 +88,55 @@ std::string DebugComponent::get_reset_reason_() {
       switch(esp_reset_reason()) {
         case ESP_RST_UNKNOWN:
           reset_reason = "Software Reset CPU (Reset reason can not be determined)";
-          break
+          break;
         case ESP_RST_POWERON:
           reset_reason = "Software Reset CPU (Reset due to power-on event)";
-          break
+          break;
         case ESP_RST_EXT:
           reset_reason = "Software Reset CPU (Reset by external pin)";
-          break
+          break;
         case ESP_RST_SW:
           reset_reason = "Software Reset CPU (Software reset via esp_restart)";
-          break
+          break;
         case ESP_RST_PANIC:
           reset_reason = "Software Reset CPU (Software reset due to exception/panic)";
-          break
+          break;
         case ESP_RST_INT_WDT:
           reset_reason = "Software Reset CPU (Reset due to interrupt watchdog)";
-          break
+          break;
         case ESP_RST_TASK_WDT:
           reset_reason = "Software Reset CPU (Reset due to task watchdog)";
-          break
+          break;
         case ESP_RST_WDT:
           reset_reason = "Software Reset CPU (Reset due to other watchdogs)";
-          break
+          break;
         case ESP_RST_DEEPSLEEP:
           reset_reason = "Software Reset CPU (Reset after exiting deep sleep mode)";
-          break
+          break;
         case ESP_RST_BROWNOUT:
           reset_reason = "Software Reset CPU (Brownout reset)";
-          break
+          break;
         case ESP_RST_SDIO:
           reset_reason = "Software Reset CPU (Reset over SDIO)";
-          break
+          break;
         case ESP_RST_USB:
           reset_reason = "Software Reset CPU (Reset by USB peripheral)";
-          break
+          break;
         case ESP_RST_JTAG:
           reset_reason = "Software Reset CPU (Reset by JTAG)";
-          break
+          break;
         case ESP_RST_EFUSE:
           reset_reason = "Software Reset CPU (Reset due to efuse error)";
-          break
+          break;
         case ESP_RST_PWR_GLITCH:
           reset_reason = "Software Reset CPU (Reset due to power glitch detected)";
-          break
+          break;
         case ESP_RST_CPU_LOCKUP:
           reset_reason = "Software Reset CPU (Reset due to CPU lock up (double exception))";
-          break
+          break;
         default:
           reset_reason = "Software Reset CPU";
-          break
+          break;
       }
       break;
     case RTCWDT_CPU_RESET:
