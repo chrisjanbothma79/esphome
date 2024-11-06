644bc7e9a2d9924b4c76b3347a10526adcc29f76
Oleg Tarasov <me@olegtarasov.email>
2024-11-06 11:32:46 +0300

    Apply suggestions from code review
    Co-authored-by: Jesse Hills <3060199+jesserockz@users.noreply.github.com>

--- esphome/components/opentherm/switch/switch.cpp
+++ esphome/components/opentherm/switch/switch.cpp
@@ -3,15 +3,17 @@
 namespace esphome {
 namespace opentherm {
 
+static const char *const TAG = "opentherm.switch";
+
 void OpenthermSwitch::write_state(bool state) { this->publish_state(state); }
 
 void OpenthermSwitch::setup() {
   auto restored = this->get_initial_state_with_restore_mode();
   bool state = false;
   if (!restored.has_value()) {
-    ESP_LOGD("opentherm.switch", "Couldn't restore state for OpenTherm switch '%s'", this->get_name().c_str());
+    ESP_LOGD(TAG, "Couldn't restore state for OpenTherm switch '%s'", this->get_name().c_str());
   } else {
-    ESP_LOGD("opentherm.switch", "Restored state for OpenTherm switch '%s': %d", this->get_name().c_str(),
+    ESP_LOGD(TAG, "Restored state for OpenTherm switch '%s': %d", this->get_name().c_str(),
              restored.value());
     state = restored.value();
   }
@@ -19,7 +21,7 @@ void OpenthermSwitch::setup() {
 }
 
 void OpenthermSwitch::dump_config() {
-  esphome::switch_::log_switch("opentherm.switch", "", "OpenTherm Switch", this);
+  LOG_SWITCH(TAG, "", "OpenTherm Switch", this);
   ESP_LOGCONFIG("opentherm.switch", "  Current state: %d", this->state);
 }
 
