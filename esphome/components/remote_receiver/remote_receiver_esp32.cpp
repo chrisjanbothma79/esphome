f95a9fda19ee20eb7519a20516936b12ab3def9f
Jonathan Swoboda <jonathan.swoboda>
2024-11-15 09:35:39 -0500

    Init all flags

--- esphome/components/remote_receiver/remote_receiver_esp32.cpp
+++ esphome/components/remote_receiver/remote_receiver_esp32.cpp
@@ -36,6 +36,10 @@ void RemoteReceiverComponent::setup() {
   channel.resolution_hz = 1 * 1000 * 1000;
   channel.mem_block_symbols = MEM_BLOCK_SIZE * this->mem_block_num_;
   channel.gpio_num = gpio_num_t(this->pin_->get_pin());
+  channel.intr_priority = 3;
+  channel.flags.invert_in = 0;
+  channel.flags.with_dma = 0;
+  channel.flags.io_loop_back = 0;
   esp_err_t error = rmt_new_rx_channel(&channel, &this->channel_);
   if (error != ESP_OK) {
     this->error_code_ = error;
