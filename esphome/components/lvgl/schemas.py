991700af2a323c91bc0d848abf1556a719bdc218
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2024-11-23 18:20:07 +1100

    Generalise image requirements

--- esphome/components/lvgl/schemas.py
+++ esphome/components/lvgl/schemas.py
@@ -342,14 +342,9 @@ FLEX_OBJ_SCHEMA = {
 }
 
 
-def bg_image_validator(value):
-    add_lv_use("img", "label")
-    return lv_image(value)
-
-
 DISP_BG_SCHEMA = cv.Schema(
     {
-        cv.Optional(df.CONF_DISP_BG_IMAGE): bg_image_validator,
+        cv.Optional(df.CONF_DISP_BG_IMAGE): lv_image,
         cv.Optional(df.CONF_DISP_BG_COLOR): lv_color,
     }
 )
