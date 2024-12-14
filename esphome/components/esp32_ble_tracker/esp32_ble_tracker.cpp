cb2fd07c815a009a2844a31ae29c9c68ca7c1c64
Tomasz Duda <tomaszduda23@gmail.com>
2024-12-14 18:59:08 +0100

    format

--- esphome/components/esp32_ble_tracker/esp32_ble_tracker.cpp
+++ esphome/components/esp32_ble_tracker/esp32_ble_tracker.cpp
@@ -685,8 +685,8 @@ void ESP32BLETracker::dump_config() {
   ESP_LOGCONFIG(TAG, "  Continuous Scanning: %s", YESNO(this->scan_continuous_));
   ESP_LOGCONFIG(TAG, "  Scanner Idle: %s", YESNO(this->scanner_idle_));
   ESP_LOGCONFIG(TAG, "  Scan End: %s", YESNO(xSemaphoreGetMutexHolder(this->scan_end_lock_) == nullptr));
-  ESP_LOGCONFIG(TAG, "  Connecting: %d, discovered %d, searching %d, disconnecting %d", connecting_, discovered_, searching_,
-             disconnecting_));
+  ESP_LOGCONFIG(TAG, "  Connecting: %d, discovered %d, searching %d, disconnecting %d", connecting_, discovered_,
+                searching_, disconnecting_);
   if (this->scan_start_fail_count_) {
     ESP_LOGCONFIG(TAG, "  Scan Start Fail Count: %d", this->scan_start_fail_count_);
   }
