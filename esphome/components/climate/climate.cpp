d6caed02c386523839741930f3a0142ad5d180f0
Joshua Slater <joshuaslater@Joshuas-MacBook-Pro.local>
2025-01-25 23:35:28 -0600

    formatting corrections

--- esphome/components/climate/climate.cpp
+++ esphome/components/climate/climate.cpp
@@ -630,35 +630,34 @@ ClimateCall ClimateDeviceRestoreState::to_call(Climate *climate) {
     }
 
 
-    template<typename T1, typename T2> bool set_alternative(optional<T1> & dst, optional<T2> & alt, const T1 &src) {
-      bool is_changed = alt.has_value();
-      alt.reset();
-      if (is_changed || dst != src) {
-        dst = src;
-        is_changed = true;
-      }
-      return is_changed;
-    }
+template<typename T1, typename T2> bool set_alternative(optional<T1> &dst, optional<T2> &alt, const T1 &src) {
+  bool is_changed = alt.has_value();
+  alt.reset();
+  if (is_changed || dst != src) {
+    dst = src;
+    is_changed = true;
+  }
+  return is_changed;
+}
+ 
+bool Climate::set_fan_mode_(ClimateFanMode mode) {
+  return set_alternative(this->fan_mode, this->custom_fan_mode, mode);
+}
+ 
+bool Climate::set_eco_mode_(ClimateEcoMode mode) { return set_alternative(this->eco_mode, mode); }
+ 
+bool Climate::set_pellet_rate_(ClimatePelletRate mode) { return set_alternative(this->pellet_rate, mode); }
+ 
+bool Climate::set_custom_fan_mode_(const std::string &mode) {
+  return set_alternative(this->custom_fan_mode, this->fan_mode, mode);
+}
+ 
+bool Climate::set_preset_(ClimatePreset preset) { return set_alternative(this->preset, this->custom_preset, preset); }
+ 
 
-    bool Climate::set_fan_mode_(ClimateFanMode mode) {
-      return set_alternative(this->fan_mode, this->custom_fan_mode, mode);
-    }
-
-    bool Climate::set_eco_mode_(ClimateEcoMode mode) { return set_alternative(this->eco_mode, mode); }
-
-    bool Climate::set_pellet_rate_(ClimatePelletRate mode) { return set_alternative(this->pellet_rate, mode); }
-
-    bool Climate::set_custom_fan_mode_(const std::string &mode) {
-      return set_alternative(this->custom_fan_mode, this->fan_mode, mode);
-    }
-
-    bool Climate::set_preset_(ClimatePreset preset) {
-      return set_alternative(this->preset, this->custom_preset, preset);
-    }
-
-    bool Climate::set_custom_preset_(const std::string &preset) {
-      return set_alternative(this->custom_preset, this->preset, preset);
-    }
+bool Climate::set_custom_preset_(const std::string &preset) {
+  return set_alternative(this->custom_preset, this->preset, preset);
+}
 
 void Climate::dump_traits_(const char *tag) {
   auto traits = this->get_traits();
