f02cd7d1cdbe3fbcd713a6723c67a069247e5196
Oliver Kleinecke <okleinecke@web.de>
2025-02-01 14:48:36 +0100

    Update mcp4461.cpp

--- esphome/components/mcp4461/mcp4461.cpp
+++ esphome/components/mcp4461/mcp4461.cpp
@@ -20,8 +20,9 @@ void Mcp4461Component::setup() {
 
 void Mcp4461Component::begin_() {
   for (uint8_t i = 0; i < 8; i++) {
-    if (this->reg_[i].enabled)
+    if (this->reg_[i].enabled) {
       this->reg_[i].state = this->get_wiper_level_(i);
+    }
   }
 }
 
@@ -45,9 +46,10 @@ void Mcp4461Component::loop() {
       if (i < 4) {
         // set terminal register changes
         if (i == 0 || i == 2) {
-          mcp4461TerminalIdx terminal_connector = mcp4461TerminalIdx::MCP4461_TERMINAL_0;
-          if (i > 0)
-            terminal_connector = mcp4461TerminalIdx::MCP4461_TERMINAL_1;
+          Mcp4461TerminalIdx terminal_connector = Mcp4461TerminalIdx::MCP4461_TERMINAL_0;
+          if (i > 0) {
+            terminal_connector = Mcp4461TerminalIdx::MCP4461_TERMINAL_1;
+          }
           uint8_t new_terminal_value = this->calc_terminal_connector_byte_(terminal_connector);
           if (new_terminal_value != this->get_terminal_register_(terminal_connector)) {
             ESP_LOGV(TAG, "updating terminal %d to new value %d", (uint8_t) terminal_connector, new_terminal_value);
@@ -60,12 +62,14 @@ void Mcp4461Component::loop() {
   }
 }
 
-uint8_t Mcp4461Component::calc_terminal_connector_byte_(mcp4461TerminalIdx terminal_connector) {
+uint8_t Mcp4461Component::calc_terminal_connector_byte_(Mcp4461TerminalIdx terminal_connector) {
   uint8_t i;
-  if ((uint8_t) terminal_connector == 0 || (uint8_t) terminal_connector == 1)
+  if (((uint8_t) terminal_connector == 0 || (uint8_t) terminal_connector == 1)) {
     i = 0;
-  else
+  }
+  else {
     i = 2;
+  }
   uint8_t new_value_byte_array[8];
   new_value_byte_array[0] = (uint8_t) this->reg_[i].terminal_b;
   new_value_byte_array[1] = (uint8_t) this->reg_[i].terminal_w;
@@ -83,15 +87,17 @@ uint8_t Mcp4461Component::calc_terminal_connector_byte_(mcp4461TerminalIdx termi
   return (uint8_t) new_value_byte;
 }
 
-void Mcp4461Component::update_terminal_register_(mcp4461TerminalIdx terminal_connector) {
-  if ((uint8_t) terminal_connector != 0 && (uint8_t) terminal_connector != 1)
+void Mcp4461Component::update_terminal_register_(Mcp4461TerminalIdx terminal_connector) {
+  if (((uint8_t) terminal_connector != 0 && (uint8_t) terminal_connector != 1)) {
     return;
+  }
   uint8_t terminal_data;
   terminal_data = this->get_terminal_register_(terminal_connector);
   ESP_LOGV(TAG, "Got terminal register %d data %0xh", terminal_connector, terminal_data);
   uint8_t wiper_index = 0;
-  if ((uint8_t) terminal_connector == 1)
+  if ((uint8_t) terminal_connector == 1) {
     wiper_index = 2;
+  }
   this->reg_[wiper_index].terminal_b = ((terminal_data >> 0) & 0x01);
   this->reg_[wiper_index].terminal_w = ((terminal_data >> 1) & 0x01);
   this->reg_[wiper_index].terminal_a = ((terminal_data >> 2) & 0x01);
@@ -117,12 +123,13 @@ uint16_t Mcp4461Component::get_status_register_() {
 
 bool Mcp4461Component::is_writing_() { return (bool) ((this->get_status_register_() >> 4) & 0x01); }
 
-uint8_t Mcp4461Component::get_terminal_register_(mcp4461TerminalIdx terminal_connector) {
+uint8_t Mcp4461Component::get_terminal_register_(Mcp4461TerminalIdx terminal_connector) {
   uint8_t reg = 0;
-  if ((uint8_t) terminal_connector == 0)
+  if ((uint8_t) terminal_connector == 0) {
     reg |= (uint8_t) Mcp4461Addresses::MCP4461_TCON0;
-  else
+  } else {
     reg |= (uint8_t) Mcp4461Addresses::MCP4461_TCON1;
+  }
   reg |= (uint8_t) Mcp4461Commands::READ;
   uint16_t buf;
   if (!this->read_byte_16(reg, &buf)) {
@@ -133,20 +140,22 @@ uint8_t Mcp4461Component::get_terminal_register_(mcp4461TerminalIdx terminal_con
   return (uint8_t) (buf & 0x00ff);
 }
 
-void Mcp4461Component::set_terminal_register_(mcp4461TerminalIdx terminal_connector, uint8_t data) {
+void Mcp4461Component::set_terminal_register_(Mcp4461TerminalIdx terminal_connector, uint8_t data) {
   uint8_t addr;
-  if ((uint8_t) terminal_connector == 0)
+  if ((uint8_t) terminal_connector == 0) {
     addr = (uint8_t) Mcp4461Addresses::MCP4461_TCON0;
-  else if ((uint8_t) terminal_connector == 1)
+  } else if ((uint8_t) terminal_connector == 1) {
     addr = (uint8_t) Mcp4461Addresses::MCP4461_TCON1;
-  else
+  } else {
     return;
+  }
   this->mcp4461_write_(addr, data);
 }
 
 void Mcp4461Component::enable_terminal_(uint8_t wiper, char terminal) {
-  if (wiper > 3)
+  if (wiper > 3) {
     return;
+  }
   ESP_LOGV(TAG, "Enabling terminal %c of wiper %d", terminal, wiper);
   switch (terminal) {
     case 'h':
@@ -170,8 +179,9 @@ void Mcp4461Component::enable_terminal_(uint8_t wiper, char terminal) {
 }
 
 void Mcp4461Component::disable_terminal_(uint8_t wiper, char terminal) {
-  if (wiper > 3)
+  if (wiper > 3) {
     return;
+  }
   ESP_LOGV(TAG, "Disabling terminal %c of wiper %d", terminal, wiper);
   switch (terminal) {
     case 'h':
@@ -218,8 +228,9 @@ uint8_t Mcp4461Component::get_wiper_address_(uint8_t wiper) {
       ESP_LOGE(TAG, "unknown wiper specified");
       return 0;
   }
-  if (nonvolatile)
+  if (nonvolatile) {
     addr = addr + 0x20;
+  }
   return addr;
 }
 
@@ -316,10 +327,12 @@ uint16_t Mcp4461Component::get_eeprom_value(MCP4461EEPRomLocation location) {
 
 void Mcp4461Component::set_eeprom_value(MCP4461EEPRomLocation location, uint16_t value) {
   uint8_t addr = 0;
-  if (value > 256)
+  if (value > 256) {
     return;
-  else if (value == 256)
+  }
+  else if (value == 256) {
     addr = 1;
+  }
   uint8_t data;
   addr |= (uint8_t) (MCP4461_EEPROM_1 + ((uint8_t) location * 0x10));
   data = (uint8_t) (value & 0x00ff);
@@ -331,10 +344,12 @@ void Mcp4461Component::set_eeprom_value(MCP4461EEPRomLocation location, uint16_t
 
 void Mcp4461Component::mcp4461_write_(uint8_t addr, uint16_t data) {
   uint8_t reg = 0;
-  if (data > 0x100)
+  if (data > 0x100) {
     return;
-  if (data > 0xFF)
+  }
+  if (data > 0xFF) {
     reg = 1;
+  }
   uint8_t value_byte;
   value_byte = (uint8_t) (data & 0x00ff);
   ESP_LOGV(TAG, "Writing value %d", data);
