81e2a8e895f7e5551f112ac5a4aa494d72007ffa
Oliver Kleinecke <okleinecke@web.de>
2025-02-01 18:05:28 +0100

    Update __init__.py

--- esphome/components/mcp4461/output/__init__.py
+++ esphome/components/mcp4461/output/__init__.py
@@ -31,7 +31,7 @@ CONFIG_SCHEMA = output.FLOAT_OUTPUT_SCHEMA.extend(
         cv.GenerateID(CONF_MCP4461_ID): cv.use_id(Mcp4461Component),
         cv.Required(CONF_CHANNEL): cv.enum(CHANNEL_OPTIONS, upper=True),
         cv.Optional(CONF_ENABLE, default=True): cv.boolean,
-        cv.Optional(CONF_INITIAL_VALUE): cv.float_range(min=0.000, max=0.256),
+        cv.Optional(CONF_INITIAL_VALUE): cv.float_range(min=0.000, max=1.0),
         cv.Optional(CONF_TERMINAL_A, default=True): cv.boolean,
         cv.Optional(CONF_TERMINAL_B, default=True): cv.boolean,
         cv.Optional(CONF_TERMINAL_W, default=True): cv.boolean,
