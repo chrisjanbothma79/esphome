478bcd6baf2c276c820edd75f51aa6e03cc17dae
Oliver Kleinecke <kleinecke.oliver@googlemail.com>
2025-02-18 14:02:46 +0100

    fix: change timer_as_bytes type to unsigned char in add_timer method

--- esphome/components/dynamic_lamp/dynamic_lamp.cpp
+++ esphome/components/dynamic_lamp/dynamic_lamp.cpp
@@ -251,7 +251,7 @@ bool DynamicLampComponent::add_timer(std::string lamp_name, bool timer_active, u
   time_t end_date = now.timestamp;
   new_timer.begin_date = begin_date;
   new_timer.end_date = end_date;
-  char* timer_as_bytes = static_cast<char*>(static_cast<void*>(&new_timer));
+  unsigned char* timer_as_bytes = static_cast<unsigned char*>(static_cast<void*>(&new_timer));
   ESP_LOGV(TAG, "Added new timer for lamp %s, active %d, mode %d, hour %d, minute %d, monday %d, tuesday %d, wednesday %d, thursday %d, friday %d, saturday %d, sunday %d",
            lamp_name.c_str(), new_timer.active, new_timer.mode, new_timer.hour, new_timer.minute, new_timer.monday, new_timer.tuesday, new_timer.wednesday,
            new_timer.thursday, new_timer.friday, new_timer.saturday, new_timer.sunday);
