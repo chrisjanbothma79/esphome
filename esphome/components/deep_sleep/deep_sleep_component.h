443e41f41fa3a6e8691e17f36d821e7182b69a23
baal86 <43853528+baal86@users.noreply.github.com>
2025-02-20 04:26:00 +0100

    Clarified comments as suggested by @DjordjeMandic
    Co-authored-by: Djordje Mandic <6750655+DjordjeMandic@users.noreply.github.com>

--- esphome/components/deep_sleep/deep_sleep_component.h
+++ esphome/components/deep_sleep/deep_sleep_component.h
@@ -48,7 +48,7 @@ struct WakeupCauseToRunDuration {
   uint32_t touch_cause;
   // Run duration if woken up by GPIO pins.
   uint32_t gpio_cause;
-  // Run duration if woken up by ext1 pins.
+  // Run duration if woken up by EXT1 pins.
   uint32_t ext1_cause;
 };
 
