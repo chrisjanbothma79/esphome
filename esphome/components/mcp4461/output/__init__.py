c2f86c6d1c1e9a14cd3e8e01c57505171500466f
Oliver Kleinecke <okleinecke@web.de>
2025-02-20 13:37:58 +0100

    Update __init__.py

--- esphome/components/mcp4461/output/__init__.py
+++ esphome/components/mcp4461/output/__init__.py
@@ -12,14 +12,14 @@ Mcp4461Wiper = mcp4461_ns.class_(
 
 Mcp4461WiperIdx = mcp4461_ns.class_("Mcp4461WiperIdx")
 CHANNEL_OPTIONS = {
-    "A": Mcp4461WiperIdx.MCP4461_WIPER_0,
-    "B": Mcp4461WiperIdx.MCP4461_WIPER_1,
-    "C": Mcp4461WiperIdx.MCP4461_WIPER_2,
-    "D": Mcp4461WiperIdx.MCP4461_WIPER_3,
-    "E": Mcp4461WiperIdx.MCP4461_WIPER_4,
-    "F": Mcp4461WiperIdx.MCP4461_WIPER_5,
-    "G": Mcp4461WiperIdx.MCP4461_WIPER_6,
-    "H": Mcp4461WiperIdx.MCP4461_WIPER_7,
+    "A": MCP4461_WIPER_0,
+    "B": MCP4461_WIPER_1,
+    "C": MCP4461_WIPER_2,
+    "D": MCP4461_WIPER_3,
+    "E": MCP4461_WIPER_4,
+    "F": MCP4461_WIPER_5,
+    "G": MCP4461_WIPER_6,
+    "H": MCP4461_WIPER_7,
 }
 
 CONF_TERMINAL_A = "terminal_a"
