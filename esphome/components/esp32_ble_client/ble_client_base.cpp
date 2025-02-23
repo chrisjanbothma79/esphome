3f47f728224e47ca886671daa86dc057b0a1ad53
J. Nick Koston <nick@koston.org>
2025-02-23 08:21:57 -0600

    tweaks

--- esphome/components/esp32_ble_client/ble_client_base.cpp
+++ esphome/components/esp32_ble_client/ble_client_base.cpp
@@ -133,7 +133,6 @@ void BLEClientBase::disconnect() {
              this->address_str_.c_str());
     return;
   }
-  ESP_LOGI(TAG, "[%d] [%s] Disconnecting.", this->connection_index_, this->address_str_.c_str());
   if (this->state_ == espbt::ClientState::CONNECTING || this->conn_id_ == UNSET_CONN_ID) {
     ESP_LOGW(TAG, "[%d] [%s] Disconnecting before connected, disconnect scheduled.", this->connection_index_,
              this->address_str_.c_str());
