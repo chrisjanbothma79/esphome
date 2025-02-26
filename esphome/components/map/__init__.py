e9ec2553d2d39b4c04d245b84cc57cec2666107f
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-02-27 09:57:35 +1100

    Better error messages

--- esphome/components/map/__init__.py
+++ esphome/components/map/__init__.py
@@ -1,8 +1,16 @@
+import difflib
+
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_FROM, CONF_ID, CONF_TO
 from esphome.core import CORE
-from esphome.cpp_generator import MockObj, VariableDeclarationExpression, add_global
+from esphome.cpp_generator import (
+    MockObj,
+    MockObjClass,
+    VariableDeclarationExpression,
+    add_global,
+)
+from esphome.loader import get_component
 
 CODEOWNERS = ["@clydebarrow"]
 MULTI_CONF = True
@@ -46,10 +54,31 @@ BASE_SCHEMA = cv.Schema(
     {
         cv.Required(CONF_ID): cv.declare_id(map_),
         cv.Required(CONF_FROM): cv.one_of(*INDEX_TYPES, lower=True),
-        cv.Required(CONF_TO): to_schema,
+        cv.Required(CONF_TO): cv.string,
     },
     extra=cv.ALLOW_EXTRA,
 )
+
+
+def get_object_type(to_):
+    """
+    Get the object type from a string. Possible formats:
+       xxx The name of a component which defines INSTANCE_TYPE
+       xxx.yyy A class name yyy defined in a component xxx
+       xxx::yyy A C++ class name defined in a component
+    """
+
+    if "::" in to_:
+        return CORE.id_classes.get(to_)
+    parts = to_.split(".")
+    if component := get_component(parts[0]):
+        if len(parts) == 1:
+            return component.instance_type
+        if len(parts) == 2:
+            result = getattr(component.module, parts[1])
+            if isinstance(result, MockObjClass):
+                return result
+    return None
 
 
 def map_schema(config):
@@ -57,17 +86,20 @@ def map_schema(config):
     if CONF_ENTRIES not in config or not isinstance(config[CONF_ENTRIES], dict):
         raise cv.Invalid("an entries list is required for a map")
     entries = config[CONF_ENTRIES]
-    to_ = config[CONF_TO]
     if len(entries) == 0:
         raise cv.Invalid("Map must have at least one entry")
+    to_ = config[CONF_TO]
     if to_ in INDEX_TYPES:
-        config[CONF_ENTRIES] = {
-            k: INDEX_TYPES[to_].validator(v) for k, v in entries.items()
-        }
+        value_type = INDEX_TYPES[to_].validator
     else:
-        config[CONF_ENTRIES] = {
-            k: cv.use_id(CORE.id_classes[to_])(v) for k, v in entries.items()
-        }
+        value_type = get_object_type(to_)
+        if value_type is None:
+            matches = difflib.get_close_matches(to_, CORE.id_classes)
+            raise cv.Invalid(
+                f"No known mappable class name matches '{to_}'; did you mean one of {', '.join(matches)}?"
+            )
+        value_type = cv.use_id(value_type)
+    config[CONF_ENTRIES] = {k: value_type(v) for k, v in entries.items()}
     return config
 
 
@@ -92,7 +124,8 @@ async def to_code(config):
             index_conversion(key): await cg.get_variable(value)
             for key, value in entries.items()
         }
-        value_type = CORE.id_classes[to_].operator("ptr")
+        value_type = get_object_type(to_)
+        value_type = get_object_type(to_).operator("ptr")
     varid = config[CONF_ID]
     varid.type = map_.template(index_type, value_type)
     var = MockObj(varid, ".")
