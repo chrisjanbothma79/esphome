cdb88c332e5c822a44895ad37081a1007b148e73
Tomasz Duda <tomaszduda23@gmail.com>
2025-02-28 11:51:12 +0100

    fix bootloader

--- esphome/components/nrf52/boards.py
+++ esphome/components/nrf52/boards.py
@@ -1,6 +1,11 @@
 from esphome.components.zephyr.const import KEY_BOOTLOADER
 
-from .const import BOOTLOADER_ADAFRUIT, BOOTLOADER_ADAFRUIT_NRF52_SD140_V6
+from .const import (
+    BOOTLOADER_ADAFRUIT,
+    BOOTLOADER_ADAFRUIT_NRF52_SD132,
+    BOOTLOADER_ADAFRUIT_NRF52_SD140_V6,
+    BOOTLOADER_ADAFRUIT_NRF52_SD140_V7,
+)
 
 
 class Section:
@@ -23,14 +28,27 @@ class Section:
 
 BOARDS_ZEPHYR = {
     "adafruit_itsybitsy_nrf52840": {
-        KEY_BOOTLOADER: [BOOTLOADER_ADAFRUIT, BOOTLOADER_ADAFRUIT_NRF52_SD140_V6]
+        KEY_BOOTLOADER: [
+            BOOTLOADER_ADAFRUIT,
+            BOOTLOADER_ADAFRUIT_NRF52_SD132,
+            BOOTLOADER_ADAFRUIT_NRF52_SD140_V6,
+            BOOTLOADER_ADAFRUIT_NRF52_SD140_V7,
+        ]
     },
 }
 
 # https://github.com/ffenix113/zigbee_home/blob/17bb7b9e9d375e756da9e38913f53303937fb66a/types/board/known_boards.go
 BOOTLOADER_CONFIG = {
+    BOOTLOADER_ADAFRUIT_NRF52_SD132: [
+        Section("empty_app_offset", 0x0, 0x26000, "flash_primary"),
+        Section("empty_after_zboss_offset", 0xF4000, 0xC000, "flash_primary"),
+    ],
     BOOTLOADER_ADAFRUIT_NRF52_SD140_V6: [
         Section("empty_app_offset", 0x0, 0x26000, "flash_primary"),
         Section("empty_after_zboss_offset", 0xF4000, 0xC000, "flash_primary"),
-    ]
+    ],
+    BOOTLOADER_ADAFRUIT_NRF52_SD140_V7: [
+        Section("empty_app_offset", 0x0, 0x27000, "flash_primary"),
+        Section("empty_after_zboss_offset", 0xF4000, 0xC000, "flash_primary"),
+    ],
 }
