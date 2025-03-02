f7074ed87d115a9b0f7446f3099b61ad91a91236
Tomasz Duda <tomaszduda23@gmail.com>
2025-03-02 15:51:41 +0100

    add doc

--- esphome/components/nrf52/boards.py
+++ esphome/components/nrf52/boards.py
@@ -38,6 +38,7 @@ BOARDS_ZEPHYR = {
 }
 
 # https://github.com/ffenix113/zigbee_home/blob/17bb7b9e9d375e756da9e38913f53303937fb66a/types/board/known_boards.go
+# https://learn.adafruit.com/introducing-the-adafruit-nrf52840-feather?view=all#hathach-memory-map
 BOOTLOADER_CONFIG = {
     BOOTLOADER_ADAFRUIT_NRF52_SD132: [
         Section("empty_app_offset", 0x0, 0x26000, "flash_primary"),
