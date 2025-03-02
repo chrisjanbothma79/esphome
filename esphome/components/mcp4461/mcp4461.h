326e1c12db36aef6a2629a4c4aabb5b5b66349a3
Oliver Kleinecke <okleinecke@web.de>
2025-03-02 21:02:27 +0100

    Update mcp4461.h

--- esphome/components/mcp4461/mcp4461.h
+++ esphome/components/mcp4461/mcp4461.h
@@ -132,7 +132,7 @@ class Mcp4461Component : public Component, public i2c::I2CDevice {
   } error_code_{MCP4461_STATUS_OK};
 
   // Converts a status to a human readable string
-  static const LogString *get_message_string_(int status) {
+  static const LogString *get_message_string(int status) {
     switch (status) {
       case MCP4461_STATUS_I2C_ERROR:
         return LOG_STR("I2C error - communication with MCP4461 failed!");
