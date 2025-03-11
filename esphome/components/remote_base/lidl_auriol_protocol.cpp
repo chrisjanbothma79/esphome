a4f213c4a6df5e1ad925ea9c110b88df9146c4c4
Gábor Poczkodi <gabest11@gmail.com>
2025-03-11 19:12:24 +0100

    fix h10515_dcf

--- esphome/components/remote_base/lidl_auriol_protocol.cpp
+++ esphome/components/remote_base/lidl_auriol_protocol.cpp
@@ -10,13 +10,12 @@ static const char *const TAG = "remote.lidl_auriol";
 #define GET_BITS(x, pos, len) (((x) >> (pos)) & ((1 << (len)) - 1))
 #define SET_BITS(x, pos, len, val) (x) |= ((uint64_t) ((val) & ((1 << (len)) - 1))) << (pos)
 
-static const uint32_t PULSE_LENGTH = 500;
-
 // temperature and rain sensor
 static const char *const MODEL_4LD631 = "4LD631";
-// TODO: unable to receive signal, does not show up in raw dump either, but rtl433 sees it
+// none of my cc1101s can receive this, but a simple srx882 can
 static const char *const MODEL_H10515_DCF = "H10515_DCF";
 // TODO: temperature, humidity, wind, rain, from the year 2005, sold under many names
+// corroded away on the tree, cannot test it anymore, sadly...
 static const char *const MODEL_H13726A = "H13726A";
 // temperature only (aka TFA-Pool)
 static const char *const MODEL_L08037A = "L08037A";
@@ -114,20 +113,18 @@ struct TIMING {
 };
 
 static bool decode_4ld631(LidlAuriolData &data) {
-  if (GET_BITS(data.code, 24, 4) != 0b1111) {
-    ESP_LOGD(TAG, "[24:27] should be 0b1111");
+  if ((uint8_t) GET_BITS(data.code, 24, 4) != 0b1111) {  // unknown, always 0b1111?
+    ESP_LOGV(TAG, "[24:27] should be 0b1111");
     return false;
   }
 
   data.id = (uint16_t) GET_BITS(data.code, 44, 8);
-  data.battery_level = (uint8_t) GET_BITS(data.code, 43, 1) == 1 ? 1 : 0;
+  data.battery_level = (uint8_t) GET_BITS(data.code, 43, 1) == 1 ? 100.0f : 0;
   // 0 = (uint8_t) GET_BITS(code, 42, 1); // ?
   data.channel = (uint8_t) GET_BITS(data.code, 40, 2);
   data.temperature = (float) ((int16_t) (GET_BITS(data.code, 28, 12) << 4)) / 160;
-  // 1111 = (uint8_t) GET_BITS(code, 24, 4); // ?
   data.humidity = (uint8_t) GET_BITS(data.code, 16, 8);
   data.rain = (float) GET_BITS(data.code, 0, 16) * 0.242f;
-
   return true;
 }
 
@@ -145,24 +142,35 @@ static bool encode_4ld631(const LidlAuriolData &data, uint64_t &code) {
 }
 
 static bool decode_h10515_dcf(LidlAuriolData &data) {
-  /*
-  // very similar to NEXUS
-  // TODO
   uint8_t chksum = 0;
   for (int i = 0; i < 8; i++) {
     chksum = (chksum + (uint8_t) GET_BITS(data.code, i * 4, 4)) & 0b1111;
   }
+  chksum = ~chksum & 0b1111;
   if ((uint8_t) GET_BITS(data.code, 32, 4) != chksum) {
-    ESP_LOGD(TAG, "chksum mismatch %x != %x", (uint8_t) GET_BITS(data.code, 32, 4), chksum);
+    ESP_LOGV(TAG, "chksum mismatch %x != %x", (uint8_t) GET_BITS(data.code, 32, 4), chksum);
     return false;
   }
-  if (GET_BITS(data.code, 8, 4) != 0b1111) {
+  if (GET_BITS(data.code, 8, 4) != 0) {  // unknown, always zero(?), sometimes 0b0010
+    ESP_LOGV(TAG, "[8:11] should be 0");
+    // return false;
+  }
+
+  data.id = (uint16_t) GET_BITS(data.code, 0, 4);  // keeps changing between resets
+  data.channel = (uint8_t) ((GET_BITS(data.code, 4, 1) << 1) | GET_BITS(data.code, 5, 1));
+  if (data.channel == 0) {
+    ESP_LOGV(TAG, "channel invalid %d", data.channel);
     return false;
   }
-  */
+  // ? == GET_BITS(data.code, 6, 2) // unknown, battery(?)
   data.temperature = (GET_BITS(data.code, 22, 1) ? -0.1 : 0.1) * GET_BITS(data.code, 12, 11);
+  data.humidity = 10.0f * GET_BITS(data.code, 28, 4) + GET_BITS(data.code, 24, 4);
+  return true;
+}
+
+static bool encode_h10515_dcf(const LidlAuriolData &data, uint64_t &code) {
+  ESP_LOGD(TAG, "TODO: encode_h10515_dcf");
   return false;
-  // TODO: return true;
 }
 
 static bool decode_h13726a(LidlAuriolData &data) {
@@ -171,13 +179,19 @@ static bool decode_h13726a(LidlAuriolData &data) {
     chksum = (chksum - (uint8_t) GET_BITS(data.code, i * 4, 4)) & 0b1111;
   }
   if ((uint8_t) GET_BITS(data.code, 32, 4) != chksum) {
-    ESP_LOGD(TAG, "chksum mismatch %x != %x", (uint8_t) GET_BITS(data.code, 32, 4), chksum);
+    ESP_LOGV(TAG, "chksum mismatch %x != %x", (uint8_t) GET_BITS(data.code, 32, 4), chksum);
     return false;
   }
+
   data.id = (uint16_t) GET_BITS(data.code, 0, 8);
   data.temperature = (float) ((int16_t) (GET_BITS(data.code, 12, 12) << 4)) / 160;
   data.humidity = 10.0f * GET_BITS(data.code, 28, 4) + GET_BITS(data.code, 24, 4);
   return true;
+}
+
+static bool encode_h13726a(const LidlAuriolData &data, uint64_t &code) {
+  ESP_LOGD(TAG, "TODO: encode_h13726a");
+  return false;
 }
 
 static bool decode_l08037a(LidlAuriolData &data) {
@@ -186,46 +200,61 @@ static bool decode_l08037a(LidlAuriolData &data) {
     chksum = (chksum + (uint8_t) GET_BITS(data.code, i * 4, 4)) & 0b1111;
   }
   if ((uint8_t) GET_BITS(data.code, 24, 4) != chksum) {
-    ESP_LOGD(TAG, "chksum mismatch %x != %x", (uint8_t) GET_BITS(data.code, 24, 4), chksum);
+    ESP_LOGV(TAG, "chksum mismatch %x != %x", (uint8_t) GET_BITS(data.code, 24, 4), chksum);
     return false;
   }
+
   data.id = (uint16_t) GET_BITS(data.code, 16, 8);
   data.channel = (uint8_t) GET_BITS(data.code, 2, 2);
-  if (data.channel == 0)
+  if (data.channel == 0) {
+    ESP_LOGV(TAG, "channel invalid %d", data.channel);
     return false;
+  }
   data.temperature = (float) ((int16_t) (GET_BITS(data.code, 4, 12) << 4)) / 160;
   return true;
+}
+
+static bool encode_l08037a(const LidlAuriolData &data, uint64_t &code) {
+  ESP_LOGD(TAG, "TODO: encode_l08037a");
+  return false;
 }
 
 static bool decode_nexus(LidlAuriolData &data) {
   // 3E806DF2F id=3E, battery ok, 10.9C, 47%
   // 0011 1110 1000 0000 0110 1101 1111 0010 1111
+  // TODO: chksum?
   if (GET_BITS(data.code, 8, 4) != 0b1111) {
+    ESP_LOGV(TAG, "[8:11] should be 0b1111");
     return false;
   }
-  // TODO: chksum?
+
   data.id = (uint16_t) GET_BITS(data.code, 28, 8);
-  data.battery_level = (uint8_t) GET_BITS(data.code, 27, 1) == 1 ? 1 : 0;
+  data.battery_level = (uint8_t) GET_BITS(data.code, 27, 1) == 1 ? 100.0f : 0;
   data.channel = (uint8_t) GET_BITS(data.code, 24, 2) + 1;
   data.temperature = (float) ((int16_t) (GET_BITS(data.code, 12, 12) << 4)) / 160;
   data.humidity = (uint8_t) GET_BITS(data.code, 0, 8);
   return true;
 }
 
+static bool encode_nexus(const LidlAuriolData &data, uint64_t &code) {
+  ESP_LOGD(TAG, "TODO: encode_nexus");
+  return false;
+}
+
 static bool encode_dummy(const LidlAuriolData &data, uint64_t &code) {
-  ESP_LOGD(TAG, "encode_dummy");
+  ESP_LOGD(TAG, "TODO: encode_dummy");
   return true;
 }
 
 static struct TIMING TIMINGS[] = {
     {
-        {PULSE_LENGTH, 8, 1, 2, 1, 4, 1, true},
+        {500, 8, 1, 2, 1, 4, 1, true},
         {
             {MODEL_4LD631, 52, 7, false, decode_4ld631, encode_4ld631},
         },
     },
     {
-        {PULSE_LENGTH, 1, 8, 1, 2, 1, 4, false},
+        {500, 1, 8, 1, 2, 1, 4, false},
         {
             // {MODEL_4LD631, 52, 7, false, decode_4ld631, encode_4ld631},
             {MODEL_NEXUS, 36, 8, false, decode_nexus, encode_dummy},
@@ -233,13 +262,13 @@ static struct TIMING TIMINGS[] = {
         },
     },
     {
-        {PULSE_LENGTH, 1, 18, 1, 4, 1, 8, false},
+        {500, 1, 18, 1, 4, 1, 8, false},
         {
-            {MODEL_H10515_DCF, 36, 6, true, decode_h10515_dcf, encode_dummy},
+            {MODEL_H10515_DCF, 36, 8, true, decode_h10515_dcf, encode_dummy},
         },
     },
     {
-        {PULSE_LENGTH, 1, 19, 1, 4, 1, 9, false},
+        {500, 1, 19, 1, 4, 1, 9, false},
         {
             {MODEL_L08037A, 28, 8, false, decode_l08037a, encode_dummy},
         },
@@ -252,7 +281,7 @@ void LidlAuriolProtocol::encode(RemoteTransmitData *dst, const LidlAuriolData &d
       if (data.model == p.model) {
         uint64_t code = 0;
         if (p.encode(data, code)) {
-          ESP_LOGD(TAG, "Sending: %s %" PRIx64 " id=%d b=%.0f ch=%d t=%.1f h=%d r=%.1f wd=%d ws=%.2f wg=%.2f",
+          ESP_LOGD(TAG, "%s %" PRIx64 " id=%d b=%.0f ch=%d t=%.1f h=%d r=%.1f wd=%d ws=%.2f wg=%.2f",
                    data.model.c_str(), code, data.id, data.battery_level, data.channel, data.temperature, data.humidity,
                    data.rain, data.wind_direction_degrees, data.wind_speed, data.wind_gust);
 
@@ -326,8 +355,9 @@ optional<LidlAuriolData> LidlAuriolProtocol::decode(RemoteReceiveData src) {
         data.code = code;
         if (p.decode(data)) {
           // sanity check
-          if (data.temperature >= -100 && data.temperature <= 100 && data.humidity <= 100)
+          if (data.temperature >= -100 && data.temperature <= 100 && data.humidity <= 100) {
             return data;
+          }
         }
       }
     }
@@ -337,9 +367,9 @@ optional<LidlAuriolData> LidlAuriolProtocol::decode(RemoteReceiveData src) {
 }
 
 void LidlAuriolProtocol::dump(const LidlAuriolData &data) {
-  ESP_LOGI(TAG, "Received: %s %" PRIx64 " id=%d b=%.0f ch=%d t=%.1f h=%d r=%.1f wd=%d ws=%.2f wg=%.2f",
-           data.model.c_str(), data.code, data.id, data.battery_level, data.channel, data.temperature, data.humidity,
-           data.rain, data.wind_direction_degrees, data.wind_speed, data.wind_gust);
+  ESP_LOGI(TAG, "%s %" PRIx64 " id=%d b=%.0f ch=%d t=%.1f h=%d r=%.1f wd=%d ws=%.2f wg=%.2f", data.model.c_str(),
+           data.code, data.id, data.battery_level, data.channel, data.temperature, data.humidity, data.rain,
+           data.wind_direction_degrees, data.wind_speed, data.wind_gust);
 }
 
 }  // namespace remote_base
