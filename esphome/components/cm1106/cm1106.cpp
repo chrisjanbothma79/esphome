30ac62bce6e2cfe4104fb7b3650b0f95f73e7c50
Andrew J.Swan <ajs.blackhole@gmail.com>
2025-03-25 13:26:17 +0200

    Chore: clang-tidy

--- esphome/components/cm1106/cm1106.cpp
+++ esphome/components/cm1106/cm1106.cpp
@@ -7,9 +7,9 @@ namespace esphome {
 namespace cm1106 {
 
 static const char *const TAG = "cm1106";
-static const uint8_t c_m1106_cmd_get_co2_[4] = {0x11, 0x01, 0x01, 0xED};
-static const uint8_t c_m1106_cmd_set_co2_calib_[6] = {0x11, 0x03, 0x03, 0x00, 0x00, 0x00};
-static const uint8_t c_m1106_cmd_set_co2_calib_response_[4] = {0x16, 0x01, 0x03, 0xE6};
+static const uint8_t C_M1106_CMD_GET_CO2[4] = {0x11, 0x01, 0x01, 0xED};
+static const uint8_t C_M1106_CMD_SET_CO2_CALIB[6] = {0x11, 0x03, 0x03, 0x00, 0x00, 0x00};
+static const uint8_t C_M1106_CMD_SET_CO2_CALIB_RESPONSE[4] = {0x16, 0x01, 0x03, 0xE6};
 
 uint8_t cm1106_checksum(const uint8_t *response, size_t len) {
   uint8_t crc = 0;
@@ -22,7 +22,7 @@ uint8_t cm1106_checksum(const uint8_t *response, size_t len) {
 void CM1106Component::setup() {
   ESP_LOGCONFIG(TAG, "Setting up CM1106...");
   uint8_t response[8] = {0};
-  if (!this->cm1106_write_command_(c_m1106_cmd_get_co2_, sizeof(c_m1106_cmd_get_co2_), response, sizeof(response))) {
+  if (!this->cm1106_write_command_(C_M1106_CMD_GET_CO2, sizeof(C_M1106_CMD_GET_CO2), response, sizeof(response))) {
     ESP_LOGE(TAG, "Communication with CM1106 failed!");
     this->mark_failed();
     return;
@@ -31,7 +31,7 @@ void CM1106Component::setup() {
 
 void CM1106Component::update() {
   uint8_t response[8] = {0};
-  if (!this->cm1106_write_command_(c_m1106_cmd_get_co2_, sizeof(c_m1106_cmd_get_co2_), response, sizeof(response))) {
+  if (!this->cm1106_write_command_(C_M1106_CMD_GET_CO2, sizeof(C_M1106_CMD_GET_CO2), response, sizeof(response))) {
     ESP_LOGW(TAG, "Reading data from CM1106 failed!");
     this->status_set_warning();
     return;
@@ -61,7 +61,7 @@ void CM1106Component::update() {
 
 void CM1106Component::calibrate_zero(uint16_t ppm) {
   uint8_t cmd[6];
-  memcpy(cmd, c_m1106_cmd_set_co2_calib_, sizeof(cmd));
+  memcpy(cmd, C_M1106_CMD_SET_CO2_CALIB, sizeof(cmd));
   cmd[3] = ppm >> 8;
   cmd[4] = ppm & 0xFF;
   uint8_t response[4] = {0};
@@ -73,7 +73,7 @@ void CM1106Component::calibrate_zero(uint16_t ppm) {
   }
 
   // check if correct response received
-  if (memcmp(response, c_m1106_cmd_set_co2_calib_response_, sizeof(response)) != 0) {
+  if (memcmp(response, C_M1106_CMD_SET_CO2_CALIB_RESPONSE, sizeof(response)) != 0) {
     ESP_LOGW(TAG, "Got wrong UART response from CM1106: %02X %02X %02X %02X", response[0], response[1], response[2],
              response[3]);
     this->status_set_warning();
