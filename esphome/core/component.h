8ec2e99e919d55a34316d2ecb3915ee6a4dbf5fe
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-03-28 10:51:39 +1100

    Enable setting error message with mark_failed

--- esphome/core/component.h
+++ esphome/core/component.h
@@ -118,6 +118,11 @@ class Component {
    */
   virtual void mark_failed();
 
+  void mark_failed(const char *message) {
+    this->mark_failed();
+    this->status_set_error(message);
+  }
+
   bool is_failed() const;
 
   bool is_ready() const;
