3034947fa7b3ebb6ce75669ff74dd5ad596297eb
victorclaessen <victor@victorclaessen.nl>
2025-03-31 22:15:26 +0200

    Update helpers.cpp
    Read ethernet mac (rather than wifi mac) if this esphome device uses ethernet.

--- esphome/core/helpers.cpp
+++ esphome/core/helpers.cpp
@@ -706,7 +706,11 @@ void get_mac_address_raw(uint8_t *mac) {  // NOLINT(readability-non-const-parame
   if (has_custom_mac_address()) {
     esp_efuse_mac_get_custom(mac);
   } else {
+  #if defined(USE_ETHERNET)
+    esp_read_mac(mac, ESP_MAC_ETH);
+  #else
     esp_efuse_mac_get_default(mac);
+  #endif
   }
 #endif
 }
