25d9cd7e4adc366c4687811ae6602be320aa0a42
Gábor Poczkodi <gabest11@gmail.com>
2025-04-06 15:50:58 +0200

    Z32171 crc code update

--- esphome/components/remote_base/weather_station_protocol.cpp
+++ esphome/components/remote_base/weather_station_protocol.cpp
@@ -573,27 +573,25 @@ void WeatherStationZ32171Protocol::setup() {
 }
 
 bool WeatherStationZ32171Protocol::transform(const std::vector<uint8_t> &code, WeatherStationData &data) const {
-  uint8_t msg[4];
+  uint8_t msg[5];
   for (int i = 0; i < 4; i++) {
     msg[i] = (uint8_t) get_bits(code, 32 - 8 * i, 8);
   }
-  // for CRC computation, channel bits are at the CRC position
-  msg[1] = (msg[1] & 0x0F) | (uint8_t) get_bits(code, 0, 4) << 4;
+  msg[4] = 0;
 
   uint8_t crc = 0;
   for (uint8_t b : msg) {
     crc ^= b;
     for (int bit = 0; bit < 8; bit++) {
       if (crc & 0x80) {
-        crc = (crc << 1) ^ 0x30;
+        crc = (crc << 1) ^ 0x31;
       } else {
         crc = (crc << 1);
       }
     }
   }
-  crc = (crc >> 4) ^ (uint8_t) get_bits(code, 4, 4);
-  if (crc != (uint8_t) get_bits(code, 28, 4)) {
-    ESP_LOGV(TAG, "chksum mismatch %02X %02X", (uint8_t) get_bits(code, 28, 4), crc);
+  if (crc != (uint8_t) get_bits(code, 0, 8)) {
+    ESP_LOGV(TAG, "chksum mismatch %02X %02X", (uint8_t) get_bits(code, 0, 8), crc);
     return false;
   }
 
