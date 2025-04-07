dad2cf8d6c3bc527c7972faa489ea48263f5e25e
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-04-07 11:52:43 +1000

    Limit PSRAM to variants that actually support it. Fix Arduino Octal PSRAM support

--- esphome/components/psram/__init__.py
+++ esphome/components/psram/__init__.py
@@ -3,9 +3,12 @@ import logging
 import esphome.codegen as cg
 from esphome.components.esp32 import (
     CONF_ENABLE_IDF_EXPERIMENTAL_FEATURES,
+    VARIANT_ESP32,
     add_idf_sdkconfig_option,
     get_esp32_variant,
+    only_on_variant,
 )
+from esphome.components.esp32.const import VARIANT_ESP32S2, VARIANT_ESP32S3
 import esphome.config_validation as cv
 from esphome.const import (
     CONF_ADVANCED,
@@ -59,18 +62,31 @@ def validate_psram_mode(config):
             raise cv.Invalid("PSRAM 120MHz is not supported in octal mode")
     if config[CONF_MODE] != TYPE_OCTAL and config[CONF_ENABLE_ECC]:
         raise cv.Invalid("ECC is only available in octal mode.")
+    if config[CONF_MODE] == TYPE_OCTAL:
+        variant = get_esp32_variant()
+        if variant != VARIANT_ESP32S3:
+            raise cv.Invalid(
+                f"Octal PSRAM is only supported on ESP32-S3, not {variant}"
+            )
     return config
 
 
-CONFIG_SCHEMA = cv.Schema(
-    {
-        cv.GenerateID(): cv.declare_id(PsramComponent),
-        cv.Optional(CONF_MODE, default=TYPE_QUAD): cv.enum(SPIRAM_MODES, lower=True),
-        cv.Optional(CONF_ENABLE_ECC, default=False): cv.boolean,
-        cv.Optional(CONF_SPEED, default=40e6): cv.All(
-            cv.frequency, cv.one_of(*SPIRAM_SPEEDS)
-        ),
-    }
+CONFIG_SCHEMA = cv.All(
+    cv.Schema(
+        {
+            cv.GenerateID(): cv.declare_id(PsramComponent),
+            cv.Optional(CONF_MODE, default=TYPE_QUAD): cv.enum(
+                SPIRAM_MODES, lower=True
+            ),
+            cv.Optional(CONF_ENABLE_ECC, default=False): cv.boolean,
+            cv.Optional(CONF_SPEED, default=40e6): cv.All(
+                cv.frequency, cv.one_of(*SPIRAM_SPEEDS)
+            ),
+        }
+    ),
+    only_on_variant(
+        supported=[VARIANT_ESP32, VARIANT_ESP32S3, VARIANT_ESP32S2],
+    ),
 )
 
 FINAL_VALIDATE_SCHEMA = validate_psram_mode
@@ -79,10 +95,8 @@ FINAL_VALIDATE_SCHEMA = validate_psram_mode
 async def to_code(config):
     if CORE.using_arduino:
         cg.add_build_flag("-DBOARD_HAS_PSRAM")
-        # TODO - make octal mode work with Arduino.
-        # The folling lines should do it, but results in build errors.
-        # if config[CONF_MODE] == TYPE_OCTAL:
-        #    cg.add_platformio_option("board_build.arduino.memory_type", "qio_opi")
+        if config[CONF_MODE] == TYPE_OCTAL:
+            cg.add_platformio_option("board_build.arduino.memory_type", "qio_opi")
 
     if CORE.using_esp_idf:
         add_idf_sdkconfig_option(
