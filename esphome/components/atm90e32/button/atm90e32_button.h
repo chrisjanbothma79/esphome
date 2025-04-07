21f76d3578093526d8227c30c4ed788eb76a2f9a
jdeglavina <jdeg@sugarmanstudios.com>
2025-04-07 16:44:47 -0400

    Update atm90e32_button.h

--- esphome/components/atm90e32/button/atm90e32_button.h
+++ esphome/components/atm90e32/button/atm90e32_button.h
@@ -12,7 +12,7 @@ class ATM90E32GainCalibrationButton : public button::Button, public Parented<ATM
   ATM90E32GainCalibrationButton() = default;
 
  protected:
-  void press_action();
+  void press_action() override;
 };
 
 class ATM90E32ClearGainCalibrationButton : public button::Button, public Parented<ATM90E32Component> {
@@ -20,7 +20,7 @@ class ATM90E32ClearGainCalibrationButton : public button::Button, public Parente
   ATM90E32ClearGainCalibrationButton() = default;
 
  protected:
-  void press_action();
+  void press_action() override;
 };
 
 class ATM90E32OffsetCalibrationButton : public button::Button, public Parented<ATM90E32Component> {
@@ -28,7 +28,7 @@ class ATM90E32OffsetCalibrationButton : public button::Button, public Parented<A
   ATM90E32OffsetCalibrationButton() = default;
 
  protected:
-  void press_action();
+  void press_action() override;
 };
 
 class ATM90E32ClearOffsetCalibrationButton : public button::Button, public Parented<ATM90E32Component> {
@@ -36,7 +36,7 @@ class ATM90E32ClearOffsetCalibrationButton : public button::Button, public Paren
   ATM90E32ClearOffsetCalibrationButton() = default;
 
  protected:
-  void press_action();
+  void press_action() override;
 };
 
 class ATM90E32PowerOffsetCalibrationButton : public button::Button, public Parented<ATM90E32Component> {
@@ -44,7 +44,7 @@ class ATM90E32PowerOffsetCalibrationButton : public button::Button, public Paren
   ATM90E32PowerOffsetCalibrationButton() = default;
 
  protected:
-  void press_action();
+  void press_action() override;
 };
 
 class ATM90E32ClearPowerOffsetCalibrationButton : public button::Button, public Parented<ATM90E32Component> {
@@ -52,7 +52,7 @@ class ATM90E32ClearPowerOffsetCalibrationButton : public button::Button, public 
   ATM90E32ClearPowerOffsetCalibrationButton() = default;
 
  protected:
-  void press_action();
+  void press_action() override;
 };
 
 }  // namespace atm90e32
