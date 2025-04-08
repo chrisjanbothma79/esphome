0c9848f40ea89f42a9e334d80c3ff8003075c7fb
David Woodhouse <dwmw2@infradead.org>
2025-04-08 19:45:36 +0100

    real_time_clock: remove call_setup() override

--- esphome/components/time/real_time_clock.h
+++ esphome/components/time/real_time_clock.h
@@ -38,8 +38,6 @@ class RealTimeClock : public PollingComponent {
   /// Get the current time as the UTC epoch since January 1st 1970.
   time_t timestamp_now() { return ::time(nullptr); }
 
-  void call_setup() override;
-
   void add_on_time_sync_callback(std::function<void()> callback) {
     this->time_sync_callback_.add(std::move(callback));
   };
