78e3d558dad00a5cad2e7a9b8d803aefa42efb51
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-04-08 20:45:51 +1000

    Revert formatting changes

--- esphome/components/lvgl/lvgl_esphome.h
+++ esphome/components/lvgl/lvgl_esphome.h
@@ -53,12 +53,14 @@ static const display::ColorBitness LV_BITNESS = display::ColorBitness::COLOR_BIT
 #ifdef USE_LVGL_IMAGE
 // Shortcut / overload, so that the source of an image can easily be updated
 // from within a lambda.
-inline void lv_img_set_src(lv_obj_t *obj, image::Image *image) { lv_img_set_src(obj, image->get_lv_img_dsc()); }
-inline void lv_disp_set_bg_image(lv_disp_t *disp, image::Image *image) {
+inline void lv_img_set_src(lv_obj_t *obj, esphome::image::Image *image) {
+  lv_img_set_src(obj, image->get_lv_img_dsc());
+}
+inline void lv_disp_set_bg_image(lv_disp_t *disp, esphome::image::Image *image) {
   lv_disp_set_bg_image(disp, image->get_lv_img_dsc());
 }
 
-inline void lv_obj_set_style_bg_img_src(lv_obj_t *obj, image::Image *image, lv_style_selector_t selector) {
+inline void lv_obj_set_style_bg_img_src(lv_obj_t *obj, esphome::image::Image *image, lv_style_selector_t selector) {
   lv_obj_set_style_bg_img_src(obj, image->get_lv_img_dsc(), selector);
 }
 inline void lv_canvas_draw_img(lv_obj_t *canvas, lv_coord_t x, lv_coord_t y, image::Image *image,
@@ -67,7 +69,7 @@ inline void lv_canvas_draw_img(lv_obj_t *canvas, lv_coord_t x, lv_coord_t y, ima
 }
 
 #ifdef USE_LVGL_METER
-inline lv_meter_indicator_t *lv_meter_add_needle_img(lv_obj_t *obj, lv_meter_scale_t *scale, image::Image *src,
+inline lv_meter_indicator_t *lv_meter_add_needle_img(lv_obj_t *obj, lv_meter_scale_t *scale, esphome::image::Image *src,
                                                      lv_coord_t pivot_x, lv_coord_t pivot_y) {
   return lv_meter_add_needle_img(obj, scale, src->get_lv_img_dsc(), pivot_x, pivot_y);
 }
