d7816b1daf2d5dff204ee89123984e0513d54b6a
Ben <65178153+Theempireshrikesback@users.noreply.github.com>
2025-04-12 14:16:55 -0700

    Update waveshare_epaper.h
    fixed trailing whitespaces from clipboard

--- esphome/components/waveshare_epaper/waveshare_epaper.h
+++ esphome/components/waveshare_epaper/waveshare_epaper.h
@@ -686,24 +686,24 @@ class WaveshareEPaper5P8InV2 : public WaveshareEPaper {
 class WaveshareEPaper5P7InF : public WaveshareEPaperC {
   public:
    void initialize() override;
- 
+
    void display() override;
- 
+
    void dump_config() override;
- 
+
   protected:
    int get_width_internal() override;
- 
+
    int get_height_internal() override;
- 
+
    uint32_t idle_timeout_() override;
- 
+
    void deep_sleep() override { ; }
- 
+
    bool wait_until_idle_();
- 
+
    bool deep_sleep_between_updates_{true};
- 
+
    void reset_() {
      if (this->reset_pin_ != nullptr) {
        this->reset_pin_->digital_write(true);
