3793e49a4d4437b1e1c153c0cd946330ea9cea45
J. Nick Koston <nick@koston.org>
2025-04-11 20:06:33 -1000

    string instead

--- esphome/components/api/api_frame_helper.cpp
+++ esphome/components/api/api_frame_helper.cpp
@@ -312,10 +312,9 @@ APIError APINoiseFrameHelper::state_action_() {
     const uint8_t *name_ptr = reinterpret_cast<const uint8_t *>(name.c_str());
     msg.insert(msg.end(), name_ptr, name_ptr + name.size() + 1);
     // node mac, terminated by null byte
-    uint8_t mac[6];
-    get_mac_address_raw(mac);
-    uint8_t *mac_ptr = reinterpret_cast<uint8_t *>(mac);
-    msg.insert(msg.end(), mac_ptr, mac_ptr + 6 + 1);
+    const std::string &mac = get_mac_address();
+    const uint8_t *mac_ptr = reinterpret_cast<const uint8_t *>(mac.c_str());
+    msg.insert(msg.end(), mac_ptr, mac_ptr + mac.size() + 1);
 
     aerr = write_frame_(msg.data(), msg.size());
     if (aerr != APIError::OK)
