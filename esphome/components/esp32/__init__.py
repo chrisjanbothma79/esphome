f518b924afb16cc1d64de2352a433f112d9dde81
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2025-04-19 08:42:44 -0400

    Move to 5.4.1

--- esphome/components/esp32/__init__.py
+++ esphome/components/esp32/__init__.py
@@ -251,11 +251,11 @@ ARDUINO_PLATFORM_VERSION = cv.Version(5, 4, 0)
 # The default/recommended esp-idf framework version
 #  - https://github.com/espressif/esp-idf/releases
 #  - https://api.registry.platformio.org/v3/packages/platformio/tool/framework-espidf
-RECOMMENDED_ESP_IDF_FRAMEWORK_VERSION = cv.Version(5, 3, 2)
+RECOMMENDED_ESP_IDF_FRAMEWORK_VERSION = cv.Version(5, 4, 1)
 # The platformio/espressif32 version to use for esp-idf frameworks
 #  - https://github.com/platformio/platform-espressif32/releases
 #  - https://api.registry.platformio.org/v3/packages/platformio/platform/espressif32
-ESP_IDF_PLATFORM_VERSION = cv.Version(53, 3, 11)
+ESP_IDF_PLATFORM_VERSION = cv.Version(54, 3, 20)
 
 # List based on https://registry.platformio.org/tools/platformio/framework-espidf/versions
 SUPPORTED_PLATFORMIO_ESP_IDF_5X = [
