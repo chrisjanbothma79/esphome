fd5e535e408c763b10ae069863cc5d2795e0eabb
J. Nick Koston <nick@koston.org>
2025-04-21 13:11:22 -1000

    fix double scan stop

--- esphome/components/esp32_ble_tracker/esp32_ble_tracker.cpp
+++ esphome/components/esp32_ble_tracker/esp32_ble_tracker.cpp
@@ -404,6 +404,7 @@ void ESP32BLETracker::gap_scan_start_complete_(const esp_ble_gap_cb_param_t::ble
 
 void ESP32BLETracker::gap_scan_stop_complete_(const esp_ble_gap_cb_param_t::ble_scan_stop_cmpl_evt_param &param) {
   ESP_LOGV(TAG, "gap_scan_stop_complete - status %d", param.status);
+  this->scanner_idle_ = true;  // Make sure to set this to true before releasing the lock
   xSemaphoreGive(this->scan_end_lock_);
 }
 
