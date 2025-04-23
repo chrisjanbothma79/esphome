dbe3c1731046feacf30fe08a59722095ef1bddf6
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-04-23 14:59:57 +1000

    Add ca certificate config test

--- esphome/components/http_request/__init__.py
+++ esphome/components/http_request/__init__.py
@@ -184,7 +184,7 @@ async def to_code(config):
     if CORE.is_host:
         if IS_MACOS:
             cg.add_build_flag("-I/opt/homebrew/opt/openssl/include")
-            cg.add_build_flag("-L/opt/homebrew/opt/openssl")
+            cg.add_build_flag("-L/opt/homebrew/opt/openssl/lib")
             cg.add_build_flag("-lssl")
             cg.add_build_flag("-lcrypto")
             cg.add_build_flag("-Wl,-framework,CoreFoundation")
