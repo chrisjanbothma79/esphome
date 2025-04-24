48b65b35ed5d6d1deb68dc861b00f44c7dc7fddb
Calum McConnell <calumlikesapplepie@gmail.com>
2025-04-24 00:17:42 -0400

    Rehash init.py, rephrase title

--- esphome/components/speaker_math/speaker/__init__.py
+++ esphome/components/speaker_math/speaker/__init__.py
@@ -1,34 +1,30 @@
 import esphome.codegen as cg
-from esphome.components import audio, esp32, speaker
+from esphome.components import audio, speaker
 import esphome.config_validation as cv
 from esphome.const import (
     CONF_BITS_PER_SAMPLE,
     CONF_BUFFER_DURATION,
-    CONF_FILTERS,
     CONF_ID,
     CONF_NUM_CHANNELS,
     CONF_OUTPUT_SPEAKER,
     CONF_SAMPLE_RATE,
-    CONF_TASK_STACK_IN_PSRAM,
     PLATFORM_ESP32,
 )
 from esphome.core.entity_helpers import inherit_property_from
 
 AUTO_LOAD = ["audio"]
-CODEOWNERS = ["@kahrendt"]
+CODEOWNERS = ["@calumapplepie"]
 
-resampler_ns = cg.esphome_ns.namespace("resampler")
-ResamplerSpeaker = resampler_ns.class_(
-    "ResamplerSpeaker", cg.Component, speaker.Speaker
-)
+CONF_UNSIGNED = "convert_to_unsigned"
 
-CONF_TAPS = "taps"
+speaker_math_ns = cg.esphome_ns.namespace("speaker_math")
+SpeakerMath = speaker_math_ns.class_("SpeakerMath", cg.Component, speaker.Speaker)
 
 
 def _set_stream_limits(config):
     audio.set_stream_limits(
         min_bits_per_sample=16,
-        max_bits_per_sample=32,
+        max_bits_per_sample=16,
     )(config)
 
     return config
@@ -48,26 +44,15 @@ def _validate_audio_compatability(config):
     )(config)
 
 
-def _validate_taps(taps):
-    value = cv.int_range(min=16, max=128)(taps)
-    if value % 4 != 0:
-        raise cv.Invalid("Number of taps must be divisible by 4")
-    return value
-
-
 CONFIG_SCHEMA = cv.All(
     speaker.SPEAKER_SCHEMA.extend(
         {
-            cv.GenerateID(): cv.declare_id(ResamplerSpeaker),
+            cv.GenerateID(): cv.declare_id(SpeakerMath),
             cv.Required(CONF_OUTPUT_SPEAKER): cv.use_id(speaker.Speaker),
             cv.Optional(
                 CONF_BUFFER_DURATION, default="100ms"
             ): cv.positive_time_period_milliseconds,
-            cv.SplitDefault(CONF_TASK_STACK_IN_PSRAM, esp32_idf=False): cv.All(
-                cv.boolean, cv.only_with_esp_idf
-            ),
-            cv.Optional(CONF_FILTERS, default=16): cv.int_range(min=2, max=1024),
-            cv.Optional(CONF_TAPS, default=16): _validate_taps,
+            cv.Optional(CONF_UNSIGNED, default=False): cv.boolean(),
         }
     ).extend(cv.COMPONENT_SCHEMA),
     cv.only_on([PLATFORM_ESP32]),
@@ -88,16 +73,5 @@ async def to_code(config):
 
     cg.add(var.set_buffer_duration(config[CONF_BUFFER_DURATION]))
 
-    if task_stack_in_psram := config.get(CONF_TASK_STACK_IN_PSRAM):
-        cg.add(var.set_task_stack_in_psram(task_stack_in_psram))
-        if task_stack_in_psram:
-            if config[CONF_TASK_STACK_IN_PSRAM]:
-                esp32.add_idf_sdkconfig_option(
-                    "CONFIG_SPIRAM_ALLOW_STACK_EXTERNAL_MEMORY", True
-                )
-
     cg.add(var.set_target_bits_per_sample(config[CONF_BITS_PER_SAMPLE]))
     cg.add(var.set_target_sample_rate(config[CONF_SAMPLE_RATE]))
-
-    cg.add(var.set_filters(config[CONF_FILTERS]))
-    cg.add(var.set_taps(config[CONF_TAPS]))
