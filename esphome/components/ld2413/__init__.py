5516d401fbe4a03f9d9b7f7950b88db3eff97ca1
BBart19 <barti0800@gmail.com>
2025-04-30 22:07:42 +0200

    Fix: convert to LF and remove trailing whitespace

--- esphome/components/ld2413/__init__.py
+++ esphome/components/ld2413/__init__.py
@@ -1,3 +1,1 @@
-from . import sensor
-
 CODEOWNERS = ["@BBart19"]
--- esphome/components/ld2413/sensor.py
+++ esphome/components/ld2413/sensor.py
@@ -1,12 +1,14 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
 from esphome.components import sensor, uart
-from esphome.const import CONF_ID, UNIT_CENTIMETER, ICON_RULER
+import esphome.config_validation as cv
+from esphome.const import ICON_RULER, UNIT_CENTIMETER
 
 DEPENDENCIES = ["uart"]
 
 ld2413_ns = cg.esphome_ns.namespace("ld2413")
-LD2413Sensor = ld2413_ns.class_("LD2413Sensor", sensor.Sensor, cg.PollingComponent, uart.UARTDevice)
+LD2413Sensor = ld2413_ns.class_(
+    "LD2413Sensor", sensor.Sensor, cg.PollingComponent, uart.UARTDevice
+)
 
 CONFIG_SCHEMA = (
     sensor.sensor_schema(
@@ -14,12 +16,15 @@ CONFIG_SCHEMA = (
         icon=ICON_RULER,
         accuracy_decimals=2,
     )
-    .extend({
-        cv.GenerateID(): cv.declare_id(LD2413Sensor),
-    })
+    .extend(
+        {
+            cv.GenerateID(): cv.declare_id(LD2413Sensor),
+        }
+    )
     .extend(cv.polling_component_schema("0.5s"))
     .extend(uart.UART_DEVICE_SCHEMA)
 )
+
 
 async def to_code(config):
     var = await sensor.new_sensor(config)
