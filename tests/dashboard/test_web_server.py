3915e1f0120b6ffc9483c7d845b1ba80eda77eab
Daniel Vikström <daniel@vikstrom.name>
2025-05-06 03:36:03 +0200

    Revert "Improve stability for unrelated test"
    This reverts commit 3922950951191ed3052964b000dac2651595c419.

--- tests/dashboard/test_web_server.py
+++ tests/dashboard/test_web_server.py
@@ -75,9 +75,6 @@ async def test_devices_page(dashboard: DashboardTestHelper) -> None:
     assert response.headers["content-type"] == "application/json"
     json_data = json.loads(response.body.decode())
     configured_devices = json_data["configured"]
-    if len(configured_devices) == 0:
-        assert len(configured_devices) != 0
-    else:
-        first_device = configured_devices[0]
-        assert first_device["name"] == "pico"
-        assert first_device["configuration"] == "pico.yaml"
+    first_device = configured_devices[0]
+    assert first_device["name"] == "pico"
+    assert first_device["configuration"] == "pico.yaml"
