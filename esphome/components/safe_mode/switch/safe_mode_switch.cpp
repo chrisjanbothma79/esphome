ec31681934514832209d4f0ef9e4d4420bcf9457
J. Nick Koston <nick@koston.org>
2025-05-28 23:41:01 -0500

    Update esphome/components/safe_mode/switch/safe_mode_switch.cpp

--- esphome/components/safe_mode/switch/safe_mode_switch.cpp
+++ esphome/components/safe_mode/switch/safe_mode_switch.cpp
@@ -17,7 +17,7 @@ void SafeModeSwitch::write_state(bool state) {
   this->publish_state(false);
 
   if (state) {
-    ESP_LOGI(TAG, "Restarting device...");
+    ESP_LOGI(TAG, "Restarting device in safe mode");
     this->safe_mode_component_->set_safe_mode_pending(true);
 
     // Let MQTT settle a bit
