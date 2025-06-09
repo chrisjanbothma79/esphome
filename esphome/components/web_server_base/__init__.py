e036b05a0800dd9868c8a9509563c11da5c9ea43
Jonathan Swoboda <154711427+swoboda1337@users.noreply.github.com>
2025-06-09 06:49:36 -0400

    Upgrade libraries after Arduino 3 fixes

--- esphome/components/web_server_base/__init__.py
+++ esphome/components/web_server_base/__init__.py
@@ -37,9 +37,4 @@ async def to_code(config):
             cg.add_library("FS", None)
             cg.add_library("Update", None)
         # https://github.com/esphome/ESPAsyncWebServer/blob/master/library.json
-        # cg.add_library("esphome/ESPAsyncWebServer-esphome", "3.3.0")
-        cg.add_library(
-            "ESPAsyncWebServer",
-            None,
-            "https://github.com/swoboda1337/ESPAsyncWebServer#arduino3",
-        )
+        cg.add_library("esphome/ESPAsyncWebServer-esphome", "3.4.0")
