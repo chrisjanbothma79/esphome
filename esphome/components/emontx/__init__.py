f6259cd5668e6ed69bf1e061fb7d18b12f1a9b6c
Frédéric Metrich <frederic.metrich@live.fr>
2025-06-09 21:44:21 +0200

    Auto load Json, add dependency to uart, check/set uart config.

--- esphome/components/emontx/__init__.py
+++ esphome/components/emontx/__init__.py
@@ -3,7 +3,9 @@ from esphome.components import uart
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
 
+AUTO_LOAD = ["json"]
 CODEOWNERS = ["@FredM67"]
+DEPENDENCIES = ["uart"]
 
 emontx_ns = cg.esphome_ns.namespace("emontx")
 EmonTx = emontx_ns.class_("EmonTx", cg.PollingComponent, uart.UARTDevice)
@@ -24,8 +26,18 @@ CONFIG_SCHEMA = (
             cv.GenerateID(): cv.declare_id(EmonTx),
         }
     )
-    .extend(cv.polling_component_schema("5s"))
+    .extend(cv.polling_component_schema("10s"))
     .extend(uart.UART_DEVICE_SCHEMA)
+)
+
+FINAL_VALIDATE_SCHEMA = uart.final_validate_device_schema(
+    "emontx",
+    baud_rate=115200,
+    require_tx=False,
+    require_rx=True,
+    data_bits=8,
+    parity=None,
+    stop_bits=1,
 )
 
 
