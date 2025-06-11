8fe6a323d82a9fbe266e3510e1d6afd852f78390
J. Nick Koston <nick@koston.org>
2025-06-11 13:00:55 -0500

    remove workaround

--- esphome/components/logger/logger.cpp
+++ esphome/components/logger/logger.cpp
@@ -116,7 +116,7 @@ void Logger::log_vprintf_(int level, const char *tag, int line, const __FlashStr
   if (this->baud_rate_ > 0) {
     this->write_msg_(this->tx_buffer_ + msg_start);
   }
-  this->call_log_callbacks_(level, tag, this->tx_buffer_ + msg_start);
+  this->log_callback_.call(level, tag, this->tx_buffer_ + msg_start);
 
   global_recursion_guard_ = false;
 }
@@ -127,10 +127,6 @@ inline int Logger::level_for(const char *tag) {
   if (it != this->log_levels_.end())
     return it->second;
   return this->current_level_;
-}
-
-void HOT Logger::call_log_callbacks_(int level, const char *tag, const char *msg) {
-  this->log_callback_.call(level, tag, msg);
 }
 
 Logger::Logger(uint32_t baud_rate, size_t tx_buffer_size) : baud_rate_(baud_rate), tx_buffer_size_(tx_buffer_size) {
@@ -180,7 +176,7 @@ void Logger::loop() {
                                   this->tx_buffer_size_);
       this->write_footer_to_buffer_(this->tx_buffer_, &this->tx_buffer_at_, this->tx_buffer_size_);
       this->tx_buffer_[this->tx_buffer_at_] = '\0';
-      this->call_log_callbacks_(message->level, message->tag, this->tx_buffer_);
+      this->log_callback_.call(message->level, message->tag, this->tx_buffer_);
       // At this point all the data we need from message has been transferred to the tx_buffer
       // so we can release the message to allow other tasks to use it as soon as possible.
       this->log_buffer_->release_message_main_loop(received_token);
