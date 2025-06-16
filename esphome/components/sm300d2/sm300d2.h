059b11d55ed9c6a1a5ae36e3b0ed73c1fc3f117d
Duker4444 <32688504+Duker4444@users.noreply.github.com>
2025-06-16 16:44:52 -0400

    Update sm300d2.h
    Updated the code for better handling and now processes the newer datastrings.
    You will see two new Sensors:
    
    Address : Sensor Address.
    Function: not needed but included. 
    
     Usually these are hidden since they arn't used . However they are added in the newer Hardware updates in V2 and are causing the errors seen.

--- esphome/components/sm300d2/sm300d2.h
+++ esphome/components/sm300d2/sm300d2.h
@@ -3,6 +3,7 @@
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/components/uart/uart.h"
+#include <vector> // For std::vector
 
 namespace esphome {
 namespace sm300d2 {
@@ -11,6 +12,8 @@ class SM300D2Sensor : public PollingComponent, public uart::UARTDevice {
  public:
   float get_setup_priority() const override { return setup_priority::DATA; }
 
+  void set_addr_sensor(sensor::Sensor *addr_sensor) { addr_sensor_ = addr_sensor; }
+  void set_function_sensor(sensor::Sensor *function_sensor) { function_sensor_ = function_sensor; }
   void set_co2_sensor(sensor::Sensor *co2_sensor) { co2_sensor_ = co2_sensor; }
   void set_formaldehyde_sensor(sensor::Sensor *formaldehyde_sensor) { formaldehyde_sensor_ = formaldehyde_sensor; }
   void set_tvoc_sensor(sensor::Sensor *tvoc_sensor) { tvoc_sensor_ = tvoc_sensor; }
@@ -23,8 +26,11 @@ class SM300D2Sensor : public PollingComponent, public uart::UARTDevice {
   void dump_config() override;
 
  protected:
-  uint16_t sm300d2_checksum_(uint8_t *ptr);
+  uint16_t sm300d2_checksum_(const uint8_t *ptr, uint8_t length); // Changed to const uint8_t*
+  void process_packet(const std::vector<uint8_t>& packet, bool is_new_revision);
 
+  sensor::Sensor *addr_sensor_{nullptr};
+  sensor::Sensor *function_sensor_{nullptr};
   sensor::Sensor *co2_sensor_{nullptr};
   sensor::Sensor *formaldehyde_sensor_{nullptr};
   sensor::Sensor *tvoc_sensor_{nullptr};
