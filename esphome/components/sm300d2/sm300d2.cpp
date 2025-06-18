77d578ec7d87da95b481b91a4deb278b9e40dcaf
Duker4444 <32688504+Duker4444@users.noreply.github.com>
2025-06-17 21:21:18 -0400

    Update sm300d2.cpp
    clear old vector code

--- esphome/components/sm300d2/sm300d2.cpp
+++ esphome/components/sm300d2/sm300d2.cpp
@@ -11,8 +11,6 @@ static const uint8_t SM300D2_NEW_RESPONSE_LENGTH = 19;
 static const uint32_t MAX_BUFFER_SIZE = 256;
 static const uint16_t MAX_REASONABLE_PM = 1000;
 
-std::vector<uint8_t> buffer_;
-
 void SM300D2Sensor::update() {
   while (this->available()) {
     uint8_t byte = this->read();
