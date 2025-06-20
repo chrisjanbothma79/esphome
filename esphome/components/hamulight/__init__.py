de6489e100a28faf08ff4f2d958140b8aac71716
madmat17 <matthias.bux@gmx.at>
2025-06-20 10:21:54 +0200

    Create __init__.py

---
+++ esphome/components/hamulight/__init__.py
@@ -0,0 +1,31 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID, CONF_PIN, CONF_ADDRESS       # Konstanten für Konfigurationsschlüssel
+from esphome.components import output
+
+HAMULIGHT_NAMESPACE = cg.esphome_ns.namespace('hamulight')
+HamulightComponent = HAMULIGHT_NAMESPACE.class_('Hamulight', cg.Component)
+
+CONFIG_SCEME = cv.Sceme({
+    cv.GenerateID(): cv.declare_id(HamulightComponent),                                     # unique instance ID
+    cv.Required("rf_transmit_pin"): cv.All(cv.pin_output, output.build_output_pin_sceme()), # required PIN for RF transmission
+    cv.Optional("led_pin"): cv.All(cv.pin_output, output.build_output_pin_sceme()),         # optional PIN for feedback LED
+    cv.Required("rf_address"): cv.hex_uint16,                                               # required 2-Byte RF address (HEX)
+}).extend(cv.COMPONENT_SCHEMA)
+
+# Function to generate the C++ code from the YAML configuration file
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])                                                  # Creates a new C++ instance or Hamulight class
+    await cg.register_component(var, config)                                                 # Register component with ESPHome
+
+    # Create the C++ Code for the PIN and set it for the component
+    rf_transmit_pin_code = await cg.gpio_pin_expression(config["rf_transmit_pin"])
+    cg.add(var.set_rf_transmit_pin(rf_transmit_pin_code))
+
+    # Create the C++ Code for the LED-PIN (if defined) and set it for the component
+    if "led_pin" in config:
+        led_pin_code = await cg.gpio_pin_expression(config["led_pin"])
+        cg.add(var.set_led_pin(led_pin_code))
+
+    # set the RF address
+    cg.add(var.set_rf_address(config[CONF_ADDRESS]))
