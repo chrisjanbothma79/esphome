4a8e4fe3f14fa8e1395cd43911ed1027926af1c1
Edward Firmo <94725493+edwardtfn@users.noreply.github.com>
2025-06-21 09:00:01 +0200

    Rename file

--- esphome/components/nextion/nextion_upload.cpp
+++ esphome/components/nextion/nextion_upload.cpp
@@ -6,25 +6,22 @@
 
 namespace esphome {
 namespace nextion {
-static const char *const TAG = "nextion.upload.common";
+static const char *const TAG = "nextion.upload";
 
 bool Nextion::upload_end_(bool successful) {
-  ESP_LOGD(TAG, "TFT upload done: %s", YESNO(successful));
-
   if (successful) {
-    ESP_LOGD(TAG, "Restart");
+    ESP_LOGD(TAG, "Upload successful");
     delay(1500);  // NOLINT
     App.safe_reboot();
-    delay(1500);  // NOLINT
   } else {
-    ESP_LOGE(TAG, "TFT upload failed");
+    ESP_LOGE(TAG, "Upload failed");
 
     this->is_updating_ = false;
     this->ignore_is_setup_ = false;
 
     uint32_t baud_rate = this->parent_->get_baud_rate();
     if (baud_rate != this->original_baud_rate_) {
-      ESP_LOGD(TAG, "Baud back: %" PRIu32 "->%" PRIu32, baud_rate, this->original_baud_rate_);
+      ESP_LOGD(TAG, "Baud: %" PRIu32 "->%" PRIu32, baud_rate, this->original_baud_rate_);
       this->parent_->set_baud_rate(this->original_baud_rate_);
       this->parent_->load_settings();
     }
