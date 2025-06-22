754d2874e7903c8a49f4f74795516d559d382a5e
Jesse Hills <3060199+jesserockz@users.noreply.github.com>
2025-06-23 09:21:29 +1200

    ``this->``

--- esphome/core/area.h
+++ esphome/core/area.h
@@ -6,10 +6,10 @@ namespace esphome {
 
 class Area {
  public:
-  void set_area_id(uint32_t area_id) { area_id_ = area_id; }
-  uint32_t get_area_id() { return area_id_; }
-  void set_name(const char *name) { name_ = name; }
-  const char *get_name() { return name_; }
+  void set_area_id(uint32_t area_id) { this->area_id_ = area_id; }
+  uint32_t get_area_id() { return this->area_id_; }
+  void set_name(const char *name) { this->name_ = name; }
+  const char *get_name() { return this->name_; }
 
  protected:
   uint32_t area_id_{};
--- esphome/core/device.h
+++ esphome/core/device.h
@@ -4,12 +4,12 @@ namespace esphome {
 
 class Device {
  public:
-  void set_device_id(uint32_t device_id) { device_id_ = device_id; }
-  uint32_t get_device_id() { return device_id_; }
-  void set_name(const char *name) { name_ = name; }
-  const char *get_name() { return name_; }
-  void set_area_id(uint32_t area_id) { area_id_ = area_id; }
-  uint32_t get_area_id() { return area_id_; }
+  void set_device_id(uint32_t device_id) { this->device_id_ = device_id; }
+  uint32_t get_device_id() { return this->device_id_; }
+  void set_name(const char *name) { this->name_ = name; }
+  const char *get_name() { return this->name_; }
+  void set_area_id(uint32_t area_id) { this->area_id_ = area_id; }
+  uint32_t get_area_id() { return this->area_id_; }
 
  protected:
   uint32_t device_id_{};
