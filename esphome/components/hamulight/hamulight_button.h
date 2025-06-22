799a5110560ec25b79ef5041dbe72b994abc153f
madmat17 <matthias.bux@gmx.at>
2025-06-22 15:22:23 +0200

    Update hamulight_button.h

--- esphome/components/hamulight/hamulight_button.h
+++ esphome/components/hamulight/hamulight_button.h
@@ -13,14 +13,10 @@ namespace hamulight {
  * are also triggered by ESPHome after the press_action.
  */
 class HamulightButton : public button::Button {
- public:
+ 
+public:
   void set_callback(std::function<void()> cb) { cb_ = std::move(cb); }
-  std::string get_object_id() const override {
-    // Replace spaces with underscores for valid object_id
-    std::string name = this->get_name();
-    std::replace(name.begin(), name.end(), ' ', '_');
-    return name;
-  }
+
  protected:
   void press_action() override {
     if (cb_)
