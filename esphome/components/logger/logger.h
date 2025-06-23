f0369893615f66c405bb34640faa3354df3895d3
J. Nick Koston <nick@koston.org>
2025-06-23 09:01:01 +0200

    Update esphome/components/logger/logger.h

--- esphome/components/logger/logger.h
+++ esphome/components/logger/logger.h
@@ -107,7 +107,7 @@ class Logger : public Component {
 #ifdef USE_ESPHOME_TASK_LOG_BUFFER
   void init_log_buffer(size_t total_buffer_size);
 #endif
-#if defined(USE_LOGGER_USB_CDC) || defined(USE_ESPHOME_TASK_LOG_BUFFER)
+#if defined(USE_LOGGER_USB_CDC) || defined(USE_ESP32)
   void loop() override;
 #endif
   /// Manually set the baud rate for serial, set to 0 to disable.
