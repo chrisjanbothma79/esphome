e87ca2bd5d5a99c595c145c7e1afe7884000b425
David Woodhouse <dwmw2@infradead.org>
2025-06-28 21:31:52 +0100

    Update esphome/components/mqtt/mqtt_backend_esp32.h
    Co-authored-by: J. Nick Koston <nick+github@koston.org>

--- esphome/components/mqtt/mqtt_backend_esp32.h
+++ esphome/components/mqtt/mqtt_backend_esp32.h
@@ -108,7 +108,7 @@ struct QueueElement {
 class MQTTBackendESP32 final : public MQTTBackend {
  public:
   static const size_t MQTT_BUFFER_SIZE = 4096;
-  static const size_t TASK_STACK_SIZE = 4096;
+  static const size_t TASK_STACK_SIZE = 2048;
   static const ssize_t TASK_PRIORITY = 5;
   static const uint8_t MQTT_QUEUE_LENGTH = 30;  // 30*12 bytes = 360
 
