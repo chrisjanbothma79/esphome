ec9969296334b47ff1685536548622e0c173bec1
J. Nick Koston <nick@koston.org>
2025-07-01 15:40:40 -0500

    Apply suggestions from code review

--- esphome/components/ota/__init__.py
+++ esphome/components/ota/__init__.py
@@ -25,7 +25,6 @@ CONF_ON_STATE_CHANGE = "on_state_change"
 ota_ns = cg.esphome_ns.namespace("ota")
 OTAComponent = ota_ns.class_("OTAComponent", cg.Component)
 OTAState = ota_ns.enum("OTAState")
-
 OTAAbortTrigger = ota_ns.class_("OTAAbortTrigger", automation.Trigger.template())
 OTAEndTrigger = ota_ns.class_("OTAEndTrigger", automation.Trigger.template())
 OTAErrorTrigger = ota_ns.class_("OTAErrorTrigger", automation.Trigger.template())
@@ -85,7 +84,6 @@ BASE_OTA_SCHEMA = cv.Schema(
 async def to_code(config):
     cg.add_define("USE_OTA")
 
-    # Libraries for OTA backends
     if CORE.is_esp32 and CORE.using_arduino:
         cg.add_library("Update", None)
 
