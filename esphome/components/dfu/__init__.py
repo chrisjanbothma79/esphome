4b4a63a4fdae030495488a7c28fdb9b134281ea4
Tomasz Duda <tomaszduda23@gmail.com>
2025-07-04 22:10:24 +0200

    validate config

--- esphome/components/dfu/__init__.py
+++ esphome/components/dfu/__init__.py
@@ -1,6 +1,7 @@
 import esphome.codegen as cg
 from esphome.components import output
-from esphome.components.zephyr import zephyr_add_prj_conf
+from esphome.components.zephyr import zephyr_add_prj_conf, zephyr_data
+from esphome.components.zephyr.const import BOOTLOADER_MCUBOOT, KEY_BOOTLOADER
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, PLATFORM_NRF52
 
@@ -18,6 +19,19 @@ CONFIG_SCHEMA = cv.All(
     ).extend(cv.COMPONENT_SCHEMA),
     cv.only_on(PLATFORM_NRF52),
 )
+
+
+def _validate_mcumgr(config):
+    bootloader = zephyr_data()[KEY_BOOTLOADER]
+    if bootloader == BOOTLOADER_MCUBOOT:
+        raise cv.Invalid(f"'{bootloader}' bootloader does not support DFU")
+
+
+def _final_validate(config):
+    _validate_mcumgr(config)
+
+
+FINAL_VALIDATE_SCHEMA = _final_validate
 
 
 async def to_code(config):
