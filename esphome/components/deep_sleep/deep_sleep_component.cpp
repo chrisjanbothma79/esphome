9c282798117216a3887d36f698adb1ce793e5d73
NP v/d Spek <github_mail@lumensoft.nl>
2025-07-11 00:55:40 +0200

    remove loop() mathod and use ISR to check wakup-pin chamge

--- esphome/components/deep_sleep/deep_sleep_component.cpp
+++ esphome/components/deep_sleep/deep_sleep_component.cpp
@@ -13,17 +13,18 @@ void DeepSleepComponent::setup() {
   ESP_LOGCONFIG(TAG, "Setting up Deep Sleep...");
   global_has_deep_sleep = true;
 
-  const optional<uint32_t> run_duration = get_run_duration_();
+  const optional<uint32_t> run_duration = this->get_run_duration_();
   if (run_duration.has_value()) {
     ESP_LOGI(TAG, "Scheduling Deep Sleep to start in %" PRIu32 " ms", *run_duration);
-    this->set_timeout(*run_duration, [this]() { this->begin_sleep(); });
+    this->set_timeout("DeepSleep_RunTime", *run_duration, [this]() { this->begin_sleep(); });
   } else {
     ESP_LOGD(TAG, "Not scheduling Deep Sleep, as no run duration is configured.");
   }
+  this->setup_platform_();
 }
 
 void DeepSleepComponent::dump_config() {
-  ESP_LOGCONFIG(TAG, "Setting up Deep Sleep...");
+  ESP_LOGCONFIG(TAG, "Settings for Deep Sleep...");
   if (this->sleep_duration_.has_value()) {
     uint32_t duration = *this->sleep_duration_ / 1000;
     ESP_LOGCONFIG(TAG, "  Sleep Duration: %" PRIu32 " ms", duration);
@@ -34,10 +35,7 @@ void DeepSleepComponent::dump_config() {
   this->dump_config_platform_();
 }
 
-void DeepSleepComponent::loop() {
-  if (this->next_enter_deep_sleep_)
-    this->begin_sleep();
-}
+float DeepSleepComponent::get_setup_priority() const { return setup_priority::LATE; }
 
 float DeepSleepComponent::get_loop_priority() const {
   return -100.0f;  // run after everything else is ready
@@ -48,12 +46,12 @@ void DeepSleepComponent::set_sleep_duration(uint32_t time_ms) { this->sleep_dura
 void DeepSleepComponent::set_run_duration(uint32_t time_ms) { this->run_duration_ = time_ms; }
 
 void DeepSleepComponent::begin_sleep(bool manual) {
-  if (this->prevent_ && !manual) {
-    this->next_enter_deep_sleep_ = true;
-    return;
-  }
+  this->cancel_timeout("DeepSleep_RunTime");
 
-  if (!this->prepare_to_sleep_()) {
+  if (!manual && (this->waiting_for_sleep_ || this->prevent_ > 0)) {
+    this->waiting_for_sleep_ = true;
+    ESP_LOGD(TAG, "wait for sleep.");
+
     return;
   }
 
@@ -66,11 +64,22 @@ void DeepSleepComponent::begin_sleep(bool manual) {
   this->deep_sleep_();
 }
 
-float DeepSleepComponent::get_setup_priority() const { return setup_priority::LATE; }
+void DeepSleepComponent::prevent_deep_sleep() {
+  if (this->prevent_ < 127) {
+    this->prevent_ += 1;
+  }
+  ESP_LOGD(TAG, "-- prevent count is %d.", this->prevent_);
+}
 
-void DeepSleepComponent::prevent_deep_sleep() { this->prevent_ = true; }
-
-void DeepSleepComponent::allow_deep_sleep() { this->prevent_ = false; }
+void DeepSleepComponent::allow_deep_sleep() {
+  if (this->prevent_ > 0) {
+    this->prevent_ -= 1;
+  }
+  ESP_LOGD(TAG, "++ prevent count is %d.", this->prevent_);
+  if (this->prevent_ == 0 && this->waiting_for_sleep_) {
+    this->begin_sleep(true);
+  }
+}
 
 }  // namespace deep_sleep
 }  // namespace esphome
