66be9da3431a8e594b2ea40b5ec8cc86595a1b1b
optimusprimespace <62800678+optimusprimespace@users.noreply.github.com>
2025-07-11 12:24:39 -0400

    clang-fix

--- esphome/components/hm3301/aqi_calculator.h
+++ esphome/components/hm3301/aqi_calculator.h
@@ -20,11 +20,11 @@ class AQICalculator : public AbstractAQICalculator {
 
   int index_grid_[AMOUNT_OF_LEVELS][2] = {{0, 50}, {51, 100}, {101, 150}, {151, 200}, {201, 300}, {301, 500}};
 
-  int pm2_5_calculation_grid_[AMOUNT_OF_LEVELS][2] = {{0, 9},    {10, 35},   {36, 55},  
+  int pm2_5_calculation_grid_[AMOUNT_OF_LEVELS][2] = {{0, 9},    {10, 35},   {36, 55},
                                                       {56, 125}, {126, 225}, {256, INT_MAX}};
 
-  int pm10_0_calculation_grid_[AMOUNT_OF_LEVELS][2] = {{0, 54},    {55, 154},  {155, 254}, 
-                                                       {255, 354}, {355, 424}, {425, INT_MAX}};
+  int pm10_0_calculation_grid_[AMOUNT_OF_LEVELS][2] = {{0, 54},    {55, 154},  {155, 254},
+                                                        {255, 354}, {355, 424}, {425, INT_MAX}};
 
   int calculate_index_(uint16_t value, int array[AMOUNT_OF_LEVELS][2]) {
     int grid_index = get_grid_index_(value, array);
