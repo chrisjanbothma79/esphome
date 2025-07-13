834c9f33e553df8a4b425cc91ec7ff128d23bbd2
rain714 <ch1ka73kan@gmail.com>
2025-07-13 22:44:31 +0900

    [hitachi_ac424] Support internal cleaning mode

--- esphome/components/hitachi_ac424/climate.py
+++ esphome/components/hitachi_ac424/climate.py
@@ -1,13 +1,21 @@
 import esphome.codegen as cg
 from esphome.components import climate_ir
+import esphome.config_validation as cv
 
 AUTO_LOAD = ["climate_ir"]
 
 hitachi_ac424_ns = cg.esphome_ns.namespace("hitachi_ac424")
 HitachiClimate = hitachi_ac424_ns.class_("HitachiClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(HitachiClimate)
+CONF_INTERNAL_CLEANING = "internal_cleaning"
+CONFIG_SCHEMA = climate_ir.climate_ir_with_receiver_schema(HitachiClimate).extend(
+    {
+        cv.Optional(CONF_INTERNAL_CLEANING, default=False): cv.boolean,
+    }
+)
 
 
 async def to_code(config):
-    await climate_ir.new_climate_ir(config)
+    var = await climate_ir.new_climate_ir(config)
+
+    cg.add(var.set_internal_cleaning(config[CONF_INTERNAL_CLEANING]))
