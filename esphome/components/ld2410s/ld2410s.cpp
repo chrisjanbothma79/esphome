ab96e4501e3fbf33fbe71dfdb13910f842525a20
NovakI <ivan.novak.rs@gmail.com>
2025-07-14 22:46:04 +0200

    two digit db energy values

--- esphome/components/ld2410s/ld2410s.cpp
+++ esphome/components/ld2410s/ld2410s.cpp
@@ -1006,7 +1006,7 @@ void LD2410S::update_ts_snrs_() {
   ESP_LOGI(TAG, "Gate Trigger SNR: %s", vals.c_str());
 }
 void LD2410S::update_ts_energy_values_() {
-  std::string vals = this->format_int_(this->energy_values_, 16, 3);
+  std::string vals = this->format_int_(this->energy_values_, 16, 2);
 
   if (energy_values_str_ != vals) {
     for (auto &listener : this->listeners_) {
