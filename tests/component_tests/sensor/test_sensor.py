01d104f22e9934c8312a29b0769690559dccbacf
clydeps <U5yx99dok9>
2025-07-18 15:29:50 +1000

    Add test

--- tests/component_tests/sensor/test_sensor.py
+++ tests/component_tests/sensor/test_sensor.py
@@ -12,3 +12,8 @@ def test_sensor_device_class_set(generate_main):
 
     # Then
     assert 's_1->set_device_class("voltage");' in main_cpp
+    assert (
+        "threshold_id->set_upper_threshold([=]() -> float {\n    return s_1->state;"
+        in main_cpp
+    )
+    print(main_cpp)
