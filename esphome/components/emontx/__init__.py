85ce160bc2d95ccf69e7d9662f0c749b41c21d05
Frédéric Metrich <frederic.metrich@live.fr>
2025-07-19 13:20:43 +0200

    Fix python const_ref

--- esphome/components/emontx/__init__.py
+++ esphome/components/emontx/__init__.py
@@ -229,6 +229,9 @@ async def to_code(config):
             trigger = cg.new_Pvariable(conf[CONF_TRIGGER_ID], var)
             await automation.build_automation(
                 trigger,
-                [(cg.JsonObjectConst, "json"), (cg.std_string.const_ref, "raw_json")],
+                [
+                    (cg.JsonObjectConst, "json"),
+                    (cg.std_string.const_reference(), "raw_json"),
+                ],
                 conf,
             )
