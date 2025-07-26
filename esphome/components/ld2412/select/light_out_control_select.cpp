a3fa085ac076ccc98f5a473e54e10e0f7a832ca3
J. Nick Koston <nick@koston.org>
2025-07-26 08:47:23 -1000

    use c++17 namespaces

--- esphome/components/ld2412/select/light_out_control_select.cpp
+++ esphome/components/ld2412/select/light_out_control_select.cpp
@@ -1,12 +1,10 @@
 #include "light_out_control_select.h"
 
-namespace esphome {
-namespace ld2412 {
+namespace esphome::ld2412 {
 
 void LightOutControlSelect::control(const std::string &value) {
   this->publish_state(value);
   this->parent_->set_light_out_control();
 }
 
-}  // namespace ld2412
-}  // namespace esphome
+}  // namespace esphome::ld2412
