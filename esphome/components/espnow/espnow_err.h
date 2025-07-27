d1cfb8bce7407b6722513efb779f7de1b3569ee6
NP v/d Spek <github_mail@lumensoft.nl>
2025-07-27 10:50:19 +0200

    make sure that you cant send packets to not existing peers

--- esphome/components/espnow/espnow_err.h
+++ esphome/components/espnow/espnow_err.h
@@ -5,16 +5,14 @@
 #include <esp_err.h>
 #include <esp_now.h>
 
-namespace esphome {
-namespace espnow {
+namespace esphome::espnow {
 
 static const esp_err_t ESP_ERR_ESPNOW_CMP_BASE = (ESP_ERR_ESPNOW_BASE + 20);
 static const esp_err_t ESP_ERR_ESPNOW_FAILED = (ESP_ERR_ESPNOW_CMP_BASE + 1);
 static const esp_err_t ESP_ERR_ESPNOW_OWN_PEER_ADDRESS = (ESP_ERR_ESPNOW_CMP_BASE + 2);
 static const esp_err_t ESP_ERR_ESPNOW_PAYLOAD_SIZE = (ESP_ERR_ESPNOW_CMP_BASE + 3);
 static const esp_err_t ESP_ERR_ESPNOW_PEER_NOT_SET = (ESP_ERR_ESPNOW_CMP_BASE + 4);
-
-}  // namespace espnow
-}  // namespace esphome
+static const esp_err_t ESP_ERR_ESPNOW_PEER_NOT_PAIRED = (ESP_ERR_ESPNOW_CMP_BASE + 5);
+}  // namespace esphome::espnow
 
 #endif  // USE_ESP32
