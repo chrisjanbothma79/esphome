f9c2d7e138f2a294b0bfa4fde6c7fca4925a390d
NovakI <ivan.novak.rs@gmail.com>
2025-07-28 16:53:57 +0200

    CONF

--- esphome/components/ld2410s/text_sensor/__init__.py
+++ esphome/components/ld2410s/text_sensor/__init__.py
@@ -11,10 +11,10 @@ LD2410STextSensor = ld2410s_ns.class_(
 
 FW_VERSION = "fw_version"
 
-TRIGGER_THRESHOLD_TS = "trigger_threshold_ts"
-TRIGGER_HOLD_TS = "trigger_hold_ts"
-TRIGGER_SNR_TS = "trigger_snr_ts"
-ENERGY_VALUES_TS = "energy_values_ts"
+CONF_ENERGY_VALUES = "energy_values_ts"
+CONF_TRIGGER_THRESHOLD = "trigger_threshold_ts"
+CONF_TRIGGER_HOLD = "trigger_hold_ts"
+CONF_TRIGGER_SNR = "trigger_snr_ts"
 
 CONFIG_SCHEMA = cv.All(
     cv.COMPONENT_SCHEMA.extend(
@@ -24,19 +24,19 @@ CONFIG_SCHEMA = cv.All(
             cv.Optional(FW_VERSION): text_sensor.text_sensor_schema(
                 entity_category=ENTITY_CATEGORY_DIAGNOSTIC, icon=ICON_CHIP
             ),
-            cv.Optional(TRIGGER_THRESHOLD_TS): text_sensor.text_sensor_schema(
+            cv.Optional(CONF_TRIGGER_THRESHOLD): text_sensor.text_sensor_schema(
                 entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
                 icon="mdi:tune-variant",
             ),
-            cv.Optional(TRIGGER_HOLD_TS): text_sensor.text_sensor_schema(
+            cv.Optional(CONF_TRIGGER_HOLD): text_sensor.text_sensor_schema(
                 entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
                 icon="mdi:tune-variant",
             ),
-            cv.Optional(TRIGGER_SNR_TS): text_sensor.text_sensor_schema(
+            cv.Optional(CONF_TRIGGER_SNR): text_sensor.text_sensor_schema(
                 entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
                 icon="mdi:tune-variant",
             ),
-            cv.Optional(ENERGY_VALUES_TS): text_sensor.text_sensor_schema(
+            cv.Optional(CONF_ENERGY_VALUES): text_sensor.text_sensor_schema(
                 entity_category=ENTITY_CATEGORY_DIAGNOSTIC,
                 icon="mdi:lightning-bolt",
             ),
@@ -53,20 +53,20 @@ async def to_code(config):
         sens = await text_sensor.new_text_sensor(config[FW_VERSION])
         cg.add(var.set_fw_version_text_sensor(sens))
 
-    if TRIGGER_THRESHOLD_TS in config:
-        sens = await text_sensor.new_text_sensor(config[TRIGGER_THRESHOLD_TS])
+    if CONF_TRIGGER_THRESHOLD in config:
+        sens = await text_sensor.new_text_sensor(config[CONF_TRIGGER_THRESHOLD])
         cg.add(var.set_trigger_threshold_text_sensor(sens))
 
-    if TRIGGER_HOLD_TS in config:
-        sens = await text_sensor.new_text_sensor(config[TRIGGER_HOLD_TS])
+    if CONF_TRIGGER_HOLD in config:
+        sens = await text_sensor.new_text_sensor(config[CONF_TRIGGER_HOLD])
         cg.add(var.set_trigger_hold_text_sensor(sens))
 
-    if TRIGGER_SNR_TS in config:
-        sens = await text_sensor.new_text_sensor(config[TRIGGER_SNR_TS])
+    if CONF_TRIGGER_SNR in config:
+        sens = await text_sensor.new_text_sensor(config[CONF_TRIGGER_SNR])
         cg.add(var.set_trigger_snr_text_sensor(sens))
 
-    if ENERGY_VALUES_TS in config:
-        sens = await text_sensor.new_text_sensor(config[ENERGY_VALUES_TS])
+    if CONF_ENERGY_VALUES in config:
+        sens = await text_sensor.new_text_sensor(config[CONF_ENERGY_VALUES])
         cg.add(var.set_energy_values_text_sensor(sens))
 
     ld2410s = await cg.get_variable(config[CONF_LD2410S_ID])
