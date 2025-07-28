39cb03d63e17883574afaa245c4ff753efc2c524
Jordan Zucker <jordan.zucker@gmail.com>
2025-07-28 10:45:22 -0700

    Hoping this is last check

--- esphome/components/prometheus/prometheus_handler.cpp
+++ esphome/components/prometheus/prometheus_handler.cpp
@@ -1051,7 +1051,7 @@ void PrometheusHandler::date_row_(AsyncResponseStream *stream, datetime::DateEnt
     timeinfo.tm_hour = 0;
     timeinfo.tm_min = 0;
     timeinfo.tm_sec = 0;
-    timeinfo.tm_isdst = -1;  // Let system determine DST
+    timeinfo.tm_isdst = 0;  // Force UTC (no DST)
     time_t timestamp = mktime(&timeinfo);
     stream->print(static_cast<int64_t>(timestamp));
     stream->print(F("\n"));
@@ -1149,7 +1149,7 @@ void PrometheusHandler::datetime_row_(AsyncResponseStream *stream, datetime::Dat
     timeinfo.tm_hour = obj->hour;
     timeinfo.tm_min = obj->minute;
     timeinfo.tm_sec = obj->second;
-    timeinfo.tm_isdst = -1;  // Let system determine DST
+    timeinfo.tm_isdst = 0;  // Force UTC (no DST)
     time_t timestamp = mktime(&timeinfo);
     stream->print(static_cast<int64_t>(timestamp));
     stream->print(F("\n"));
