262b028e5968bd7eb342c0e769f1c8335178eaaf
Jordan Zucker <jordan.zucker@gmail.com>
2025-07-28 10:39:58 -0700

    Why is this a mess

--- esphome/components/prometheus/prometheus_handler.cpp
+++ esphome/components/prometheus/prometheus_handler.cpp
@@ -1044,12 +1044,15 @@ void PrometheusHandler::date_row_(AsyncResponseStream *stream, datetime::DateEnt
     stream->print(relabel_name_(obj).c_str());
     stream->print(F("\"} "));
     // Data itself - convert to epoch seconds
-    ESPTime date_time = obj->state_as_esptime();
-    date_time.hour = 0;
-    date_time.minute = 0;
-    date_time.second = 0;
-    date_time.recalc_timestamp_utc();
-    stream->print(static_cast<int64_t>(date_time.timestamp));
+    struct tm timeinfo = {};
+    timeinfo.tm_year = obj->year - 1900;
+    timeinfo.tm_mon = obj->month - 1;
+    timeinfo.tm_mday = obj->day;
+    timeinfo.tm_hour = 0;
+    timeinfo.tm_min = 0;
+    timeinfo.tm_sec = 0;
+    time_t timestamp = mktime(&timeinfo);
+    stream->print(static_cast<int64_t>(timestamp));
     stream->print(F("\n"));
   } else {
     // Invalid state
@@ -1138,9 +1141,15 @@ void PrometheusHandler::datetime_row_(AsyncResponseStream *stream, datetime::Dat
     stream->print(relabel_name_(obj).c_str());
     stream->print(F("\"} "));
     // Data itself - convert to epoch seconds
-    ESPTime date_time = obj->state_as_esptime();
-    date_time.recalc_timestamp_utc();
-    stream->print(static_cast<int64_t>(date_time.timestamp));
+    struct tm timeinfo = {};
+    timeinfo.tm_year = obj->year - 1900;
+    timeinfo.tm_mon = obj->month - 1;
+    timeinfo.tm_mday = obj->day;
+    timeinfo.tm_hour = obj->hour;
+    timeinfo.tm_min = obj->minute;
+    timeinfo.tm_sec = obj->second;
+    time_t timestamp = mktime(&timeinfo);
+    stream->print(static_cast<int64_t>(timestamp));
     stream->print(F("\n"));
   } else {
     // Invalid state
