cbdcde5af72b519016eae02b94f6f4b89ca33ecf
miro32890912 <81681423+mirozbiro@users.noreply.github.com>
2025-07-30 04:04:42 +1000

    Update uln2003_pump.h
    Removing switch

--- esphome/components/uln2003/uln2003_pump.h
+++ esphome/components/uln2003/uln2003_pump.h
@@ -1,18 +1,16 @@
 #pragma once
 
 #include "esphome/core/component.h"
-#include "esphome/components/switch/switch.h"
 #include "esphome/core/gpio.h"
 
 namespace esphome {
 namespace uln2003 {
 
-class ULN2003Pump : public Component, public switch_::Switch {
+class ULN2003Pump : public Component, {
  public:
   void set_control_pin(GPIOPin *pin) { this->control_pin_ = pin; }
 
   void setup() override;
-  void dump_config() override;
   void write_state(bool state) override;
 
  protected:
