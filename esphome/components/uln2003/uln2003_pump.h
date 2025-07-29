24566b1bf5f9c2ddaf4564ea8a616d8bc6e0715d
miro32890912 <81681423+mirozbiro@users.noreply.github.com>
2025-07-30 04:23:27 +1000

    Update uln2003_pump.h

--- esphome/components/uln2003/uln2003_pump.h
+++ esphome/components/uln2003/uln2003_pump.h
@@ -1,12 +1,13 @@
 #pragma once
 
 #include "esphome/core/component.h"
+#include "esphome/components/switch/switch.h"
 #include "esphome/core/gpio.h"
 
 namespace esphome {
 namespace uln2003 {
 
-class ULN2003Pump : public Component, {
+class ULN2003Pump : public switch_::Switch, public Component, {
  public:
   void set_control_pin(GPIOPin *pin) { this->control_pin_ = pin; }
 
