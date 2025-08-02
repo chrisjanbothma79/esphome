d2734289098835122dbea74748be672d2cc16d74
clydebarrow <2366188+clydebarrow@users.noreply.github.com>
2025-08-02 20:28:25 +1000

    Add Waveshare 4"

--- esphome/components/mipi_rgb/models/waveshare.py
+++ esphome/components/mipi_rgb/models/waveshare.py
@@ -1,4 +1,5 @@
 from esphome.components.mipi import DriverChip
+from esphome.components.mipi_rgb.models.st7701s import st7701s
 from esphome.config_validation import UNDEFINED
 
 wave_4_3 = DriverChip(
@@ -36,4 +37,27 @@ wave_4_3.extend(
     vsync_back_porch=16,
     vsync_front_porch=16,
     vsync_pulse_width=4,
+)
+
+st7701s.extend(
+    "WAVESHARE-4-480x480",
+    data_rate="2MHz",
+    spi_mode="MODE3",
+    color_order="BGR",
+    pixel_mode="18bit",
+    width=480,
+    height=480,
+    invert_colors=True,
+    cs_pin=42,
+    de_pin=40,
+    hsync_pin=38,
+    vsync_pin=39,
+    pclk_pin=41,
+    pclk_frequency="12MHz",
+    pclk_inverted=False,
+    data_pins={
+        "red": [46, 3, 8, 18, 17],
+        "green": [14, 13, 12, 11, 10, 9],
+        "blue": [5, 45, 48, 47, 21],
+    },
 )
