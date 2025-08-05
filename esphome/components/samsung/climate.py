6ec8d8e2246cee283f75f8c848c3bd9c79bba040
tibi <72254544+TiberiuC39@users.noreply.github.com>
2025-08-05 17:03:38 +0300

    Update component to use climate_schema(...)

--- esphome/components/samsung/climate.py
+++ esphome/components/samsung/climate.py
@@ -10,9 +10,7 @@ AUTO_LOAD = ["climate_ir"]
 samsung_ns = cg.esphome_ns.namespace("samsung")
 SamsungClimate = samsung_ns.class_("SamsungClimate", climate_ir.ClimateIR)
 
-CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
-    {cv.GenerateID(): cv.declare_id(SamsungClimate)}
-)
+CONFIG_SCHEMA = climate_ir.climare_ir_with_receiver_schema(SamsungClimate)
 
 
 async def to_code(config):
