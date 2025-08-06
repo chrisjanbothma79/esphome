2827fa6261a4a9f409a03084229af772224a38fd
zastrixarundell <luka@bodroza.com>
2025-08-06 17:07:26 +0200

    Set output_power for BK72XX chips
    Looking at the [BK7231T COB Hardware Design Guideline](https://developer.tuya.com/en/docs/iot/bk7231t-cob-hardware-design-guideline?id=Ka97u6dz0h8b8) the maximum output power is `+17 dBm`.
    Also after looking at the [BK7231N datasheet](https://images.tuyacn.com/smart/Hardware_Developer/%E8%8A%AF%E7%89%87%E8%B5%84%E6%96%99/BK7231N_Datasheet_V1.0.pdf) (with machine translations) it seems like 17 dBm is the most commonly used option.
    
    Personally I did have stability issues with BK7231T chips not able to connect to WiFi without a hard 20 minute poweroff and after changing the max output power to 17 dBm did seems to help with stability and latency.

--- esphome/components/wifi/__init__.py
+++ esphome/components/wifi/__init__.py
@@ -315,7 +315,7 @@ CONFIG_SCHEMA = cv.All(
             ): cv.enum(WIFI_POWER_SAVE_MODES, upper=True),
             cv.Optional(CONF_FAST_CONNECT, default=False): cv.boolean,
             cv.Optional(CONF_USE_ADDRESS): cv.string_strict,
-            cv.SplitDefault(CONF_OUTPUT_POWER, esp8266=20.0): cv.All(
+            cv.SplitDefault(CONF_OUTPUT_POWER, esp8266=20.0, bk72xx=17.0): cv.All(
                 cv.decibel, cv.float_range(min=8.5, max=20.5)
             ),
             cv.SplitDefault(CONF_ENABLE_BTM, esp32_idf=False): cv.All(
