144be8d7eb0e55569aeed4cb5fa6103485badf9c
aanban <stiller-gast@gmx.de>
2025-08-08 23:20:57 +0200

    add dyson decoder

---
+++ esphome/components/remote_base/dyson_protocol.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "remote_base.h"
+
+#include <cinttypes>
+
+namespace esphome {
+namespace remote_base {
+
+struct DysonData {
+  uint16_t code;  // the button, e.g. power, swing, fan++, ...
+  uint8_t roll;   // the rolling index counter
+  bool operator==(const DysonData &rhs) const { return code == rhs.code; }
+};
+
+class DysonProtocol : public RemoteProtocol<DysonData> {
+ public:
+  void encode(RemoteTransmitData *dst, const DysonData &data) override;
+  optional<DysonData> decode(RemoteReceiveData src) override;
+  void dump(const DysonData &data) override;
+};
+
+DECLARE_REMOTE_PROTOCOL(Dyson)
+
+template<typename... Ts> class DysonAction : public RemoteTransmitterActionBase<Ts...> {
+ public:
+  TEMPLATABLE_VALUE(uint16_t, code)
+  TEMPLATABLE_VALUE(uint8_t, roll)
+
+  void encode(RemoteTransmitData *dst, Ts... x) override {
+    DysonData data{};
+    data.code = this->code_.value(x...);
+    data.roll = this->roll_.value(x...);
+    DysonProtocol().encode(dst, data);
+  }
+};
+
+}  // namespace remote_base
+}  // namespace esphome
