c2dd27c403c52fdf9b66b8b6d497a08da898c42c
NovakI <ivan.novak.rs@gmail.com>
2025-08-09 12:02:04 +0000

    aligning

--- esphome/components/ld2410s/sensor/ld2410s_calibration_progress_sensor.h
+++ esphome/components/ld2410s/sensor/ld2410s_calibration_progress_sensor.h
@@ -9,12 +9,12 @@ namespace ld2410s {
 class LD2410SCalibrationProgressSensor : public LD2410SListener, public Component, public sensor::Sensor {
  public:
   void set_calibration_progress_sensor(sensor::Sensor *sensor) { this->calibration_progress_sensor_ = sensor; }
-  void on_calibration_progress(uint16_t progress) override {
-    if (this->calibration_progress_sensor_ != nullptr) {
-      if (this->calibration_progress_sensor_->get_state() != progress) {
-        this->calibration_progress_sensor_->publish_state(progress);
-      }
-    }
+  void on_calibration_progress(uint16_t progress) override{
+      // if (this->calibration_progress_sensor_ != nullptr) {
+      //   if (this->calibration_progress_sensor_->get_state() != progress) {
+      //     this->calibration_progress_sensor_->publish_state(progress);
+      //   }
+      // }
   };
 
  private:
