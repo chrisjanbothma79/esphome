0c2b1ec83255f3e11c38948d66d93eac56c3703f
NovakI <ivan.novak.rs@gmail.com>
2025-08-09 09:58:27 +0000

    alingning subcomponents binary sensor

--- esphome/components/ld2410s/binary_sensor/__init__.py
+++ esphome/components/ld2410s/binary_sensor/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 from esphome.components import binary_sensor
 import esphome.config_validation as cv
-from esphome.const import CONF_HAS_TARGET, CONF_ID, DEVICE_CLASS_OCCUPANCY
+from esphome.const import CONF_HAS_TARGET, DEVICE_CLASS_OCCUPANCY
 
 from .. import CONF_LD2410S_ID, LD2410S, ld2410s_ns
 
@@ -29,13 +29,10 @@ CONFIG_SCHEMA = {
 
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    if CONF_HAS_TARGET in config:
-        sens = await binary_sensor.new_binary_sensor(config[CONF_HAS_TARGET])
-        cg.add(var.set_presence_sensor(sens))
-    if HAS_CALIBRATION_RUNNING in config:
-        sens = await binary_sensor.new_binary_sensor(config[HAS_CALIBRATION_RUNNING])
-        cg.add(var.set_calibration_update_sensor(sens))
     ld2410s = await cg.get_variable(config[CONF_LD2410S_ID])
-    cg.add(ld2410s.register_listener(var))
+    if has_target_config := config.get(CONF_HAS_TARGET):
+        sens = await binary_sensor.new_binary_sensor(has_target_config)
+        cg.add(ld2410s.set_presence_sensor(sens))
+    if calibration_running_config := config.get(HAS_CALIBRATION_RUNNING):
+        sens = await binary_sensor.new_binary_sensor(calibration_running_config)
+        cg.add(ld2410s.set_calibration_update_sensor(sens))
