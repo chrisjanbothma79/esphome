0e80897264cfe830510e3c21c2bfcfa344922b5e
NovakI <ivan.novak.rs@gmail.com>
2025-08-09 12:08:23 +0000

    aligning sensor

--- esphome/components/ld2410s/sensor/ld2410s_calibration_progress_sensor.h
+++ esphome/components/ld2410s/sensor/ld2410s_calibration_progress_sensor.h
@@ -6,20 +6,20 @@
 namespace esphome {
 namespace ld2410s {
 
-// class LD2410SCalibrationProgressSensor : public LD2410SListener, public Component, public sensor::Sensor {
-//  public:
-//   void set_calibration_progress_sensor(sensor::Sensor *sensor) { this->calibration_progress_sensor_ = sensor; }
-//   // void on_calibration_progress(uint16_t progress) override{
-//   //     if (this->calibration_progress_sensor_ != nullptr) {
-//   //       if (this->calibration_progress_sensor_->get_state() != progress) {
-//   //         this->calibration_progress_sensor_->publish_state(progress);
-//   //       }
-//   //     }
-//   // };
+class LD2410SCalibrationProgressSensor : public LD2410SListener, public Component, public sensor::Sensor {
+ public:
+  void set_calibration_progress_sensor(sensor::Sensor *sensor) { this->calibration_progress_sensor_ = sensor; }
+  // void on_calibration_progress(uint16_t progress) override{
+  //     if (this->calibration_progress_sensor_ != nullptr) {
+  //       if (this->calibration_progress_sensor_->get_state() != progress) {
+  //         this->calibration_progress_sensor_->publish_state(progress);
+  //       }
+  //     }
+  // };
 
-//  private:
-//   sensor::Sensor *calibration_progress_sensor_{nullptr};
-// };
+ private:
+  sensor::Sensor *calibration_progress_sensor_{nullptr};
+};
 
 }  // namespace ld2410s
 }  // namespace esphome
