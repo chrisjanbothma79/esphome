ce6d71e942dcc05963e05d9cae51d31d275248fa
J. Nick Koston <nick@koston.org>
2025-08-14 23:38:54 -0500

    Update esphome/components/esp32_ble/ble_event.h

--- esphome/components/esp32_ble/ble_event.h
+++ esphome/components/esp32_ble/ble_event.h
@@ -5,7 +5,6 @@
 #include <cstddef>  // for offsetof
 #include <cstring>  // for memcpy
 #include <vector>
-#include <memory>  // for std::unique_ptr
 #include <esp_gap_ble_api.h>
 #include <esp_gattc_api.h>
 #include <esp_gatts_api.h>
