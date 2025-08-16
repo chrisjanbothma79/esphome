3304d95f4ad48e64aab8428b40b69973f3d2c3b6
Rodrigoah <Rodrigoah@users.noreply.github.com>
2025-08-16 20:42:06 +0200

    Update climate.py to link to hitachi 168bit class

--- esphome/components/Hitachi_168bit/climate.py
+++ esphome/components/Hitachi_168bit/climate.py
@@ -6,24 +6,26 @@ from esphome.const import CONF_ID, CONF_MODEL
 AUTO_LOAD = ["climate_ir"]
 CODEOWNERS = ["@Rodrigoah"]
 
-whirlpool_ns = cg.esphome_ns.namespace("whirlpool")
-WhirlpoolClimate = whirlpool_ns.class_("WhirlpoolClimate", climate_ir.ClimateIR)
+# Must match C++ namespace and class names
+hitachi_ns = cg.esphome_ns.namespace("hitachi_168bit")
+HitachiClimate = hitachi_ns.class_("hitachi_168bitClimate", climate_ir.ClimateIR)
 
-Model = whirlpool_ns.enum("Model")
+Model = hitachi_ns.enum("Model")
+
 MODELS = {
-    "DG11J1-3A": Model.MODEL_DG11J1_3A,
     "DG11J1-91": Model.MODEL_DG11J1_91,
+    "DG11J1-3A": Model.MODEL_DG11J1_3A,
 }
 
 CONFIG_SCHEMA = climate_ir.CLIMATE_IR_WITH_RECEIVER_SCHEMA.extend(
     {
-        cv.GenerateID(): cv.declare_id(WhirlpoolClimate),
-        cv.Optional(CONF_MODEL, default="DG11J1-3A"): cv.enum(MODELS, upper=True),
+        cv.GenerateID(): cv.declare_id(HitachiClimate),
+        # keep case as-is; values include a hyphen
+        cv.Optional(CONF_MODEL, default="DG11J1-91"): cv.one_of(*MODELS.keys(), lower=False),
     }
 )
-
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await climate_ir.register_climate_ir(var, config)
-    cg.add(var.set_model(config[CONF_MODEL]))
+    cg.add(var.set_model(MODELS[config[CONF_MODEL]]))
